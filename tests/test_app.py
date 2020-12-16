51f407d6805cf707c487a1fbca3ef7fb1cd62628
mezdahun <mezeydavid@gmail.com>
2020-12-16 20:40:50 +0100

    Tested start_vision_stream

--- tests/test_app.py
+++ tests/test_app.py
@@ -9,3 +9,13 @@ class AppTest(TestCase):
         with self.assertLogs('visualswarm.app', level='INFO') as cm:
             app.health()
             self.assertEqual(cm.output, ['INFO:visualswarm.app:VisualSwarm application OK!'])
+
+    @mock.patch('visualswarm.app.Process')
+    @mock.patch('visualswarm.app.Queue')
+    def test_start_vision_stream(self, mockQueue, mockProcess):
+        if FAKE_STATUS:
+            mp = mockProcess.return_value
+            app.start_vision_stream()
+            self.assertEqual(mp.start.call_count, 2)
+            self.assertEqual(mp.join.call_count, 2)
+            self.assertEqual(mockQueue.call_count, 2)
