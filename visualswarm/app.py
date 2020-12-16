be5f66b97d761993b6bf94d6a0e39f044ad34745
mezdahun <mezeydavid@gmail.com>
2020-12-16 17:16:04 +0100

    Fix

--- visualswarm/app.py
+++ visualswarm/app.py
@@ -24,7 +24,7 @@ def start_vision_stream():
     raw_vision_stream = Queue()
     high_level_vision_stream = Queue()
     raw_vision = Process(target=vacquire.raw_vision, args=(raw_vision_stream,))
-    high_level_vision = Process(target=vacquire.high_level_vision, args=(raw_vision_stream, high_level_vision_stream,))
+    high_level_vision = Process(target=vprocess.high_level_vision, args=(raw_vision_stream, high_level_vision_stream,))
     raw_vision.start()
     high_level_vision.start()
     raw_vision.join()
