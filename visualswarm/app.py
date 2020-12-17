84c7768d7c339e10543a3d07048fc39874c3eec5
mezdahun <mezeydavid@gmail.com>
2020-12-17 09:16:47 +0100

    test boilerplates

--- visualswarm/app.py
+++ visualswarm/app.py
@@ -21,6 +21,7 @@ def health():
 
 def start_vision_stream():
     """Start the visual stream of the Pi"""
+    print(__name__)
     raw_vision_stream = Queue()
     high_level_vision_stream = Queue()
     raw_vision = Process(target=vacquire.raw_vision, args=(raw_vision_stream,))
@@ -29,3 +30,6 @@ def start_vision_stream():
     high_level_vision.start()
     raw_vision.join()
     high_level_vision.join()
+
+
+print(f'name outside: {__name__}')
