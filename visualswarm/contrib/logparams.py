5216d52cf4aef80f202b2bd7d4a097af745b86a7
mezdahun <mezeydavid@gmail.com>
2020-12-17 10:15:31 +0100

    Added colored logging
    to contrib

---
+++ visualswarm/contrib/logparams.py
@@ -0,0 +1,17 @@
+"""
+@author: mezdahun
+@description: Logging related parameters
+"""
+
+
+# ANSI escape characters to color logging and make it more understandable
+class BColors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
