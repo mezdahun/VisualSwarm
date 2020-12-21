952f61ceb83cd296c319cff418b0f0c3048173fe
mezdahun <mezeydavid@gmail.com>
2020-12-21 11:07:47 +0100

    Changed color params

--- visualswarm/contrib/segmentation.py
+++ visualswarm/contrib/segmentation.py
@@ -7,14 +7,13 @@ from numpy import uint8
 
 # Change parameters here
 TARGET_RGB_COLOR = (25, 50, 85)
-HSV_HUE_RANGE = 10
-SV_MINIMUM = 100
+HSV_HUE_RANGE = 15
+SV_MINIMUM = 75
 SV_MAXIMUM = 255
 
 # Calculating secondary parameters (Do not change)
 R, B, G = TARGET_RGB_COLOR
 TARGET_HSV_COLOR = cvtColor(uint8([[[B, G, R]]]), COLOR_BGR2HSV)
-print(TARGET_HSV_COLOR)
 
 HSV_LOW = uint8([TARGET_HSV_COLOR[0][0][0]-HSV_HUE_RANGE, SV_MINIMUM, SV_MINIMUM])
 HSV_HIGH = uint8([TARGET_HSV_COLOR[0][0][0]+HSV_HUE_RANGE, SV_MINIMUM, SV_MINIMUM])
