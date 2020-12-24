a25d0bebe0a00def95b8b4cc350b1628098abbd4
mezdahun <mezeydavid@gmail.com>
2020-12-24 11:58:41 +0100

    play with denoising

--- visualswarm/vision/vprocess.py
+++ visualswarm/vision/vprocess.py
@@ -95,6 +95,7 @@ def high_level_vision(raw_vision_stream, high_level_vision_stream):
         if segmentation.SHOW_VISION_STREAMS or segmentation.FIND_COLOR_INTERACTIVE:
             # cv2.imshow("Raw", cv2.resize(img, (160, 120)))
             # cv2.imshow("Processed", cv2.resize(blurred, (160, 129)))
+            pass
         if segmentation.FIND_COLOR_INTERACTIVE:
             cv2.imshow("Segmentation Parameters", color_sample)
 
