6ecdbb6ea17ba4fa50fca4f53f31b309d10e5651
mezdahun <mezeydavid@gmail.com>
2021-01-03 14:54:25 +0100

    Started projection

--- visualswarm/vision/vprocess.py
+++ visualswarm/vision/vprocess.py
@@ -6,7 +6,7 @@ import logging
 import cv2
 import numpy as np
 import matplotlib
-matplotlib.use('GTKAgg')
+matplotlib.use('GTK3Agg')
 import matplotlib.pyplot as plt
 
 from visualswarm.contrib import segmentation, projection
