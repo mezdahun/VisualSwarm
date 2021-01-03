a9a759df634ad18f33b22abe3b4192d342ba6ee9
mezdahun <mezeydavid@gmail.com>
2021-01-03 14:38:05 +0100

    Started projection

--- visualswarm/vision/vprocess.py
+++ visualswarm/vision/vprocess.py
@@ -114,6 +114,7 @@ def FOV_extraction(high_level_vision_stream, FOV_stream):
         (img, mask, frame_id) = high_level_vision_stream.get()
         cropped_image = mask[projection.H_MARGIN:-projection.H_MARGIN, projection.W_MARGIN:-projection.W_MARGIN]
         projection_field = np.max(cropped_image, axis=0)
+        print(projection_field.shape)
         plt.plot(projection_field)
         # cv2.imshow("Projection", cropped_image)
         # cv2.waitKey(1)
