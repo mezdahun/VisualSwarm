82b509bf4582f7ab8b64d49725fbdf464abe1a68
mezdahun <mezeydavid@gmail.com>
2021-01-13 10:36:55 +0100

    Added algorithm related parameters
    as in article

---
+++ visualswarm/contrib/flockparams.py
@@ -0,0 +1,18 @@
+"""
+@author: mezdahun
+@description: Parameters related to flocking algorithm.
+"""
+
+# Velocity Parameters
+GAM = 0.8
+V0 = 100
+ALP0 = 0.5
+ALP1 = 10
+ALP2 = 0
+
+# Heading Vector Parameters
+BET0 = 0.5
+BET1 = 10
+Bet2 = 0
+
+SAVE_CONTROL_PARAMS = True
