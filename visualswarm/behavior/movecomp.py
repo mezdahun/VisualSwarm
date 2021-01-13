df30d75a2706ac6bf154337f67032a17d7c5b080
mezdahun <mezeydavid@gmail.com>
2021-01-13 10:37:32 +0100

    Added masin functions to calculate velocity

--- visualswarm/behavior/movecomp.py
+++ visualswarm/behavior/movecomp.py
@@ -3,3 +3,48 @@
 @description: Submodule to implement main behavioral/movement computations defined by
 https://advances.sciencemag.org/content/6/6/eaay0792
 """
+import logging
+
+import numpy as np
+from scipy import integrate
+
+from visualswarm.contrib import flockparams
+
+# using main logger
+logger = logging.getLogger('visualswarm.app')
+
+
+def dPhi_V_of(Phi, V):
+    """Calculating derivative of VPF according to Phi visual angle at a given timepoint t"""
+    dPhi_V = np.diff(V, prepend=0) / np.diff(Phi, prepend=0)
+    return dPhi_V
+
+
+def dt_V_of(t, joined_V):
+    """Calculating the temporal derivative of VPF to all Phi visual angles"""
+    dt_V = np.diff(joined_V, axis=0, prepend=0) / np.diff(t, prepend=0)
+    return dt_V
+
+
+def comp_velocity(vel_now, phi, V_now, t_now=None, V_prev=None, t_prev=None):
+    """Calculating the velocity difference of the agent according the main algorithm"""
+    # Deriving over t
+    if V_prev is None or t_prev is None or t_now is None:
+        logger.debug('Movement calculation called with NONE as time-related parameters.')
+        t_vec = np.hstack((t_prev, t_now))
+        joined_V = np.vstack((V_prev, t_prev))
+        dt_V = dt_V_of(t_vec, joined_V)
+    else:
+        dt_V = np.zeros(len(phi))
+
+    # Deriving over Phi
+    dPhi_V = dPhi_V_of(phi, V_now)
+
+    # Calculating series expansion of functional G
+    G_vel = flockparams.ALP0 * (-V_now + \
+                                flockparams.ALP1 * np.square(dPhi_V) + \
+                                flockparams.ALP2 * dt_V)
+
+    # Calculating change in velocity
+    dvel = flockparams.GAM * (flockparams.V0 - vel_now) + integrate.trapz(np.cos(phi) * G_vel, phi)
+    return dvel
