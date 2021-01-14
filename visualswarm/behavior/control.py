c6821fe8c609ade536c675f94fae2cd4b41abb17
mezdahun <mezeydavid@gmail.com>
2021-01-14 12:11:34 +0100

    debug

--- visualswarm/behavior/control.py
+++ visualswarm/behavior/control.py
@@ -45,7 +45,7 @@ def VPF_to_behavior(VPF_stream, control_stream):
             time = datetime.datetime.utcnow()
 
             # generating data to dump in db
-            field_dict = {"agent_velocity": v,
+            field_dict = {"agent_velocity": dv,
                           "heading_angle": dpsi}
 
             # format the data as a single measurement for influx
