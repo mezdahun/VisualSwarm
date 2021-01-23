b9a5ccf3b354394bef95e96e012e87c7a7947566
mezdahun <mezeydavid@gmail.com>
2021-01-23 15:28:50 +0100

    Testing flocking params change

--- visualswarm/contrib/flockparams.py
+++ visualswarm/contrib/flockparams.py
@@ -4,14 +4,14 @@
 """
 
 # Velocity Parameters
-GAM = 0.1
+GAM = 0.2
 V0 = 0
-ALP0 = 0.8
+ALP0 = 1
 ALP1 = 1.2
 ALP2 = 0
 
 # Heading Vector Parameters
-BET0 = 0.8
+BET0 = 0.5
 BET1 = 1.2
 BET2 = 0
 
