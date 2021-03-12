14eae77ed5581cf3345a8a0f61ff1432d71c4969
mezdahun <mezeydavid@gmail.com>
2021-03-12 10:15:05 +0100

    fix bug in motorinterface

--- visualswarm/control/motorinterface.py
+++ visualswarm/control/motorinterface.py
@@ -1,10 +1,11 @@
 import subprocess
 from visualswarm.contrib import control
 
+
 def asebamedulla_init():
     """Establishing initial connection with the Thymio robot on a predefined interface
         Args: None
         Vars: visualswarm.control.THYMIO_DEVICE_PORT: serial port on which the robot is available for the Pi
         Returns: None
     """
-    subprocess.run(['asebamedulla', f'"ser:device={control.THYMIO_DEVICE_PORT}"']
+    subprocess.run(['asebamedulla', f'"ser:device={control.THYMIO_DEVICE_PORT}"'])
