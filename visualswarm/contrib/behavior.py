b22f6591f9f787886332c76408a90b66b472dad1
mezdahun <mezeydavid@gmail.com>
2021-03-18 15:30:10 +0100

    investigating parameters

--- visualswarm/contrib/behavior.py
+++ visualswarm/contrib/behavior.py
@@ -6,12 +6,12 @@
 # Velocity Parameters
 GAM = 0.2
 V0 = 0
-ALP0 = 0.5
-ALP1 = 0.015
+ALP0 = 0.5  # overall responsiveness of velocity change (acceleration "speed")
+ALP1 = 0.015  # ~ 1 / equilibrium distance
 ALP2 = 0
 
 # Heading Vector Parameters
-BET0 = 0.1
+BET0 = 0.3  # overall responsiveness of heading change (turning "speed")
 BET1 = 0.015
 BET2 = 0
 
