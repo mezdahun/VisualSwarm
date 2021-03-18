446542d03fbcbf24a4788bc9bf853b158925fd38
mezdahun <mezeydavid@gmail.com>
2021-03-18 15:33:51 +0100

    Deleting unused parameters

--- visualswarm/contrib/behavior.py
+++ visualswarm/contrib/behavior.py
@@ -6,7 +6,7 @@
 # Velocity Parameters
 GAM = 0.2
 V0 = 0
-ALP0 = 0.5  # overall responsiveness of velocity change (acceleration "speed")
+ALP0 = 0.5  # overall speed scale (limited by possible motor speed)
 ALP1 = 0.015  # ~ 1 / equilibrium distance
 ALP2 = 0
 
@@ -15,10 +15,3 @@ BET0 = 0.3  # overall responsiveness of heading change (turning "speed")
 BET1 = 0.015
 BET2 = 0
 
-# Normalizing Velocity
-V_MAX_ALG = 6000
-V_MAX_PHYS = 400
-
-# Normalizing dpsi
-DPSI_MAX_ALG = 700
-DPSI_MAX_PHYS = 1
