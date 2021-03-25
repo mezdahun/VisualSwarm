b536dacd226c136200b7d153983890602836a190
mezdahun <mezeydavid@gmail.com>
2021-03-25 10:18:10 +0100

    Added parameter B

--- visualswarm/contrib/control.py
+++ visualswarm/contrib/control.py
@@ -7,3 +7,6 @@ THYMIO_DEVICE_PORT = "/dev/ttyACM0"
 
 # Motor scale correction to put the motor scales into the right region
 MOTOR_SCALE_CORRECTION = 100
+
+# Distance between 2 contact points of wheel and ground in m
+B = 0.11
