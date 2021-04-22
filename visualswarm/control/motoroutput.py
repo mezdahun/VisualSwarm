f6e31eb40578ea9acf0076185ca06a9ce6bfa4be
mezdahun <mezeydavid@gmail.com>
2021-04-22 09:45:49 +0200

    log passed values

--- visualswarm/control/motoroutput.py
+++ visualswarm/control/motoroutput.py
@@ -224,7 +224,8 @@ def control_thymio(control_stream, motor_control_mode_stream, emergency_stream, 
                     (emergency_mode, proximity_values) = emergency_stream.get_nowait()
                     logger.info(f"{emergency_mode}, prox: {proximity_values}")
                 except Empty:
-                    pass
+                    emergency_mode = False
+                    proximity_values = []
 
                 if not emergency_mode:
                     if movement_mode == "BEHAVE":
