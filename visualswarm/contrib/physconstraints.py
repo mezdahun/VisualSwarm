b7bb0823e5e0c49fe4669a4e5e923642eea586c2
mezdahun <mezeydavid@gmail.com>
2021-04-22 11:21:46 +0200

    measure bwd speed

--- visualswarm/contrib/physconstraints.py
+++ visualswarm/contrib/physconstraints.py
@@ -11,3 +11,8 @@ ROT_MULTIPLIER = 0.386
 # values to 50, 50 the robot will move fwd with the velocity of ca 50*FWD_MULTIPLIER mm/second.
 # this value will depend on the physical environment, i.e. the quality of the floor, etc.
 FWD_MULTIPLIER = 0.31
+
+# this parameter transforms a motor value pair of (-n, -n) to mm/seconds. as an example if you set your motor
+# values to -50, -50 the robot will move bwd with the velocity of ca 50*BWD_MULTIPLIER mm/second.
+# this value will depend on the physical environment, i.e. the quality of the floor, etc.
+BWD_MULTIPLIER = 0.325
