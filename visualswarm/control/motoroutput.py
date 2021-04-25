e2f270b261d733fbe6d35c4caf9d72b047a07d19
mezdahun <mezeydavid@gmail.com>
2021-04-25 10:12:02 +0200

    Added speed up behavior when only back sensors are active

--- visualswarm/control/motoroutput.py
+++ visualswarm/control/motoroutput.py
@@ -295,8 +295,8 @@ def speed_up_robot(network, additional_motor_speed, emergency_stream, protocol_t
             until the proximity sensors are free, after which all recursively called avoid_obstacle methods will return.
     """
     # first getting current motor values
-    v_left_curr = network.GetVariable("thymio-II", "motor.left.target")
-    v_right_curr = network.GetVariable("thymio-II", "motor.right.target")
+    v_left_curr = network.GetVariable("thymio-II", "motor.left.target")[0]
+    v_right_curr = network.GetVariable("thymio-II", "motor.right.target")[0]
     logger.info(v_left_curr)
     logger.info(v_right_curr)
 
