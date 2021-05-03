a367c9da11614fd2a872c923d1bfec90842e5758
David <mezeydavid@gmail.com>
2021-05-03 10:39:21 +0200

    Parametrize sensor frequency

--- visualswarm/contrib/simulation.py
+++ visualswarm/contrib/simulation.py
@@ -8,3 +8,6 @@ import os
 # Simulation switch, set true to use package from WeBots as a controller package
 # Pass it as an environmental variable (in in venv, use a custom activate.bat with additional env vars in it)
 ENABLE_SIMULATION = bool(int(os.getenv('ENABLE_SIMULATION', '0')))
+
+# Update frequency of sensors
+UPFREQ_PROX_HORIZONTAL = 10  #in Hz
