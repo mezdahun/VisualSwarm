e5684391782c4a5d84e391219c3e7ae04e246fc7
David <mezeydavid@gmail.com>
2021-05-06 09:42:49 +0200

    Introduced switch between threading and multiprocessing

--- visualswarm/contrib/simulation.py
+++ visualswarm/contrib/simulation.py
@@ -11,3 +11,11 @@ ENABLE_SIMULATION = bool(int(os.getenv('ENABLE_SIMULATION', '0')))
 
 # Update frequency of sensors
 UPFREQ_PROX_HORIZONTAL = 10  #in Hz
+
+# Switch to use either multithreading or multiprocessing module during
+# webots simulations. Advantage with multiprocessing module is that it
+# can be scaled and distributed on e.g. a cluster of machines, although
+# this is not yet implemented. If multithreading is used webots auto-
+# matically limits the available rescources for the simulation, therefore
+# it is safer but slower. Deafult value is safe mode, using threading.
+SPARE_RESCOURCES = bool(int(os.getenv('SPARE_RESCOURCES', '1')))
