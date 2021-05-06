e1f17d25cd8424476c530a9319aa27f42338b9d2
David <mezeydavid@gmail.com>
2021-05-06 12:54:32 +0200

    change params

--- visualswarm/contrib/behavior.py
+++ visualswarm/contrib/behavior.py
@@ -6,11 +6,11 @@
 # Velocity Parameters
 GAM = 0.9  # irl 0.2
 V0 = 0.4
-ALP0 = 0.55  # overall speed scale (limited by possible motor speed)
+ALP0 = 0.55  # overall speed scale (limited by possible motor speed) : irl 0.5
 ALP1 = 0.010  # ~ 1 / equilibrium distance : irl 0.015
 ALP2 = 0
 
 # Heading Vector Parameters
-BET0 = 0.5  # overall responsiveness of heading change (turning "speed")
+BET0 = 0.75  # overall responsiveness of heading change (turning "speed") : irl 0.5
 BET1 = 0.015
 BET2 = 0
