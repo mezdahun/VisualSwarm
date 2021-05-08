59131f0bdb0cca94cebcd7de278c332963d1f801
David <mezeydavid@gmail.com>
2021-05-08 17:10:18 +0200

    change plotting for saved data

--- data/webots/scripts/process_run.py
+++ data/webots/scripts/process_run.py
@@ -15,13 +15,93 @@ def plot_run_orientation(data_folder, robot_name, run_number):
     plt.show()
 
 
-def plot_run_position(data_folder, robot_name, run_number):
+def plot_run_position(data_folder, robot_name, run_numbers, legends=None):
     """Plotting equilibrium distance according to an experimental data dict defined above"""
-    position_array = np.load(
-        os.path.join(data_folder, robot_name, run_number, f'{robot_name}_run{run_number}_pos.npy'))
-    fig, ax = plt.subplots(1, 1, figsize=[10, 8])
-    plt.scatter(position_array[:, 0], position_array[:, 2])
-    plt.title(f'Position of {robot_name}')
+    if not isinstance(run_numbers, list):
+        run_numbers = [run_numbers]
+
+    if legends is not None:
+        if not isinstance(legends, list):
+            legends = [legends]
+
+    fig, ax = plt.subplots(2, 2, figsize=[10, 8])
+
+    for run_number in run_numbers:
+        position_array = np.load(
+            os.path.join(data_folder, robot_name, run_number, f'{robot_name}_run{run_number}_pos.npy'))
+
+        fake_time = np.linspace(0, 1, len(position_array))
+
+        plt.axes(ax[0, 0])
+        plt.scatter(position_array[:, 0], position_array[:, 2])
+
+        plt.axes(ax[1, 0])
+        plt.plot(fake_time, position_array[:, 0])
+
+        plt.axes(ax[1, 1])
+        plt.plot(fake_time, position_array[:, 2])
+
+    plt.axes(ax[0, 0])
+    plt.title(f'Trajectory of {robot_name}')
     plt.xlabel('timestep [n]')
     plt.ylabel('position [x, y]')
+    if legends is not None:
+        plt.legend(legends)
+
+    plt.axes(ax[1, 0])
+    plt.title(f'{robot_name} x coordinate')
+    plt.xlabel('timestep [n]')
+    plt.ylabel('position [x]')
+    if legends is not None:
+        plt.legend(legends)
+
+    plt.axes(ax[1, 1])
+    plt.title(f'{robot_name} y coordinate')
+    plt.xlabel('timestep [n]')
+    plt.ylabel('position [y]')
+    if legends is not None:
+        plt.legend(legends)
+
+    plt.show()
+
+
+def plot_run_position_pendulum(data_folder, robot_name, run_numbers, legends=None):
+    """Plotting equilibrium distance according to an experimental data dict defined above"""
+    if not isinstance(run_numbers, list):
+        run_numbers = [run_numbers]
+
+    if legends is not None:
+        if not isinstance(legends, list):
+            legends = [legends]
+
+    fig, ax = plt.subplots(2, 1, figsize=[10, 8])
+
+    for run_number in run_numbers:
+        position_array = np.load(
+            os.path.join(data_folder, robot_name, run_number, f'{robot_name}_run{run_number}_pos.npy'))
+
+        fake_time = np.linspace(0, 1, len(position_array))
+
+        plt.axes(ax[0])
+        plt.plot(fake_time, position_array[:, 0])
+
+        plt.axes(ax[1])
+        plt.plot(fake_time, position_array[:, 2])
+
+    plt.suptitle('Pendulum movement for different simulation timesteps')
+
+    plt.axes(ax[0])
+    plt.title(f'{robot_name} x coordinate')
+    plt.xlabel('simulation time [AU]')
+    plt.ylabel('position [x]')
+    if legends is not None:
+        plt.legend(legends)
+
+    plt.axes(ax[1])
+    plt.title(f'{robot_name} y coordinate')
+    plt.xlabel('simulation time [AU]')
+    plt.ylabel('position [y]')
+    if legends is not None:
+        plt.legend(legends)
+
     plt.show()
