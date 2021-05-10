f8c255c97b0fe04a7e132f225d74697f1962c77c
David <mezeydavid@gmail.com>
2021-05-10 09:35:00 +0200

    latest changes on process_run

--- data/webots/scripts/process_run.py
+++ data/webots/scripts/process_run.py
@@ -1,6 +1,18 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import os
+import pickle
+
+
+def load_VSWRM_data(path):
+    array2return = []
+    with open(path, 'rb') as f:
+        while True:
+            try:
+                array2return.append(pickle.load(f))
+            except EOFError:
+                break
+    return np.array(array2return)
 
 
 def plot_run_orientation(data_folder, robot_name, run_number):
@@ -65,7 +77,8 @@ def plot_run_position(data_folder, robot_name, run_numbers, legends=None):
     plt.show()
 
 
-def plot_run_position_pendulum(data_folder, robot_name, run_numbers, legends=None, suptitle=None, simulation_time=None):
+def plot_run_position_pendulum(data_folder, robot_name, run_numbers, legends=None, suptitle=None, simulation_time=None,
+                               with_orientation=False):
     """Plotting equilibrium distance according to an experimental data dict defined above"""
     if not isinstance(run_numbers, list):
         run_numbers = [run_numbers]
@@ -74,7 +87,10 @@ def plot_run_position_pendulum(data_folder, robot_name, run_numbers, legends=Non
         if not isinstance(legends, list):
             legends = [legends]
 
-    fig, ax = plt.subplots(2, 1, figsize=[10, 8])
+    if not with_orientation:
+        fig, ax = plt.subplots(2, 1, figsize=[10, 8])
+    else:
+        fig, ax = plt.subplots(3, 1, figsize=[10, 8])
 
     for run_number in run_numbers:
         position_array = np.load(
@@ -90,6 +106,12 @@ def plot_run_position_pendulum(data_folder, robot_name, run_numbers, legends=Non
         plt.axes(ax[1])
         plt.plot(fake_time, position_array[:, 2])
 
+        if with_orientation:
+            or_array = np.load(
+                os.path.join(data_folder, robot_name, run_number, f'{robot_name}_run{run_number}_or.npy'))
+            plt.axes(ax[2])
+            plt.plot(fake_time, or_array)
+
     if suptitle is None:
         plt.suptitle('Pendulum movement for different simulation timesteps')
     else:
@@ -101,7 +123,7 @@ def plot_run_position_pendulum(data_folder, robot_name, run_numbers, legends=Non
         plt.xlabel('simulation time [V-sec]')
     else:
         plt.xlabel('simulation time [AU]')
-    plt.ylabel('position [x]')
+    plt.ylabel('position [m]')
     if legends is not None:
         plt.legend(legends)
 
@@ -111,8 +133,80 @@ def plot_run_position_pendulum(data_folder, robot_name, run_numbers, legends=Non
         plt.xlabel('simulation time [V-sec]')
     else:
         plt.xlabel('simulation time [AU]')
-    plt.ylabel('position [y]')
+    plt.ylabel('position [m]')
     if legends is not None:
         plt.legend(legends)
+
+    plt.axes(ax[2])
+    plt.title(f'{robot_name} orientation')
+    if simulation_time is not None:
+        plt.xlabel('simulation time [V-sec]')
+    else:
+        plt.xlabel('simulation time [AU]')
+    plt.ylabel('orientation [rad]')
+    if legends is not None:
+        plt.legend(legends)
+
+    plt.show()
+
+
+def plot_position(data_folder, robot_name, run_numbers, legends=None, suptitle=None, with_orientation=False):
+    """Plotting position and orientation of a single robot possibly for multiple runs"""
+    if not isinstance(run_numbers, list):
+        run_numbers = [run_numbers]
+
+    if legends is not None:
+        if not isinstance(legends, list):
+            legends = [legends]
+
+    if not with_orientation:
+        fig, ax = plt.subplots(2, 1, figsize=[10, 8])
+    else:
+        fig, ax = plt.subplots(3, 1, figsize=[10, 8])
+
+    for run_number in run_numbers:
+        position_array = load_VSWRM_data(
+            os.path.join(data_folder, robot_name, run_number, f'{robot_name}_run{run_number}_pos.npy'))
+
+        fake_time = position_array[:, 0] / 1000
+
+        plt.axes(ax[0])
+        plt.plot(fake_time, position_array[:, 1])
+
+        plt.axes(ax[1])
+        plt.plot(fake_time, position_array[:, 3])
+
+        if with_orientation:
+            or_array = load_VSWRM_data(
+                os.path.join(data_folder, robot_name, run_number, f'{robot_name}_run{run_number}_or.npy'))
+            plt.axes(ax[2])
+            plt.plot(fake_time, or_array[:, 1])
+
+    if suptitle is None:
+        plt.suptitle(f'Position of robot {robot_name}')
+    else:
+        plt.suptitle(suptitle)
+
+    plt.axes(ax[0])
+    plt.title(f'{robot_name} x coordinate')
+    plt.xlabel('simulation time [V-sec]')
+    plt.ylabel('position [m]')
+    if legends is not None:
+        plt.legend(legends)
+
+    plt.axes(ax[1])
+    plt.title(f'{robot_name} y coordinate')
+    plt.xlabel('simulation time [V-sec]')
+    plt.ylabel('position [m]')
+    if legends is not None:
+        plt.legend(legends)
+
+    if with_orientation:
+        plt.axes(ax[2])
+        plt.title(f'{robot_name} orientation')
+        plt.xlabel('simulation time [V-sec]')
+        plt.ylabel('orientation [rad]')
+        if legends is not None:
+            plt.legend(legends)
 
     plt.show()
