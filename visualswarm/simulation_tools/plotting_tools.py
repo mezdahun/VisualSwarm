fc6ac3d9a462d2af66abbae55aafa28cd6a876d9
David <mezeydavid@gmail.com>
2021-05-12 08:26:51 +0200

    Cleanup

--- visualswarm/simulation_tools/plotting_tools.py
+++ visualswarm/simulation_tools/plotting_tools.py
@@ -85,3 +85,58 @@ def plot_position(data_folder, robot_name, run_numbers, legends=None, suptitle=N
         plt.close()
     else:
         plt.show()
+
+
+def calculate_distance(p1, p2):
+    squared_dist = np.sum((p1 - p2) ** 2, axis=0)
+    dist = np.sqrt(squared_dist)
+    return dist
+
+
+def plot_distance(data_folder, robot_names, run_number, legends=None, suptitle=None, with_orientation=False,
+                  with_save=False, from_fixed_pos=None):
+    if isinstance(run_number, list):
+        raise Exception('More than a single run number is passed, using the first one')
+        run_number = run_number[0]
+
+    num_robots = len(robot_names)
+
+    if num_robots > 2:
+        raise Exception('Plotting method implemented for 2 robots only')
+
+    robot_positions = []
+
+    for robot_name in robot_names:
+        position_array = load_VSWRM_data(
+            os.path.join(data_folder, robot_name, run_number, f'{robot_name}_run{run_number}_pos.npy'))
+        robot_positions.append(position_array)
+
+    if from_fixed_pos is None:
+        # distance between moving robots
+
+        distance_array = np.zeros(np.min((len(robot_positions[0][:, 0]), len(robot_positions[1][:, 0]))))
+        fake_time = np.zeros(np.min((len(robot_positions[0][:, 0]), len(robot_positions[1][:, 0]))))
+
+        for t in range(len(distance_array)):
+            distance = calculate_distance(robot_positions[0][t, 1::], robot_positions[1][t, 1::])
+            distance_array[t] = distance
+            fake_time[t] = robot_positions[0][t, 0]
+
+        fig, ax = plt.subplots(1, 1, figsize=[10, 8])
+        plt.plot(fake_time, distance)
+
+    else:
+
+        distance_array = []
+        fake_time = []
+
+        robot_position = robot_positions[0]
+        for t in range(len(robot_position)):
+            distance = calculate_distance(robot_position[t, 1::], from_fixed_pos)
+            distance_array.append(distance)
+            fake_time.append(robot_position[t, 0])
+
+        fig, ax = plt.subplots(1, 1, figsize=[10, 8])
+        plt.plot(distance_array)
+
+    plt.show()
