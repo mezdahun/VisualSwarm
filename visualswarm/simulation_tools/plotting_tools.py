196abe5ffae26e0270b484b4bcd11597245d48db
David <mezeydavid@gmail.com>
2021-05-13 07:43:17 +0200

    Added data processing tools

--- visualswarm/simulation_tools/plotting_tools.py
+++ visualswarm/simulation_tools/plotting_tools.py
@@ -5,18 +5,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import os
+from visualswarm.simulation_tools import data_tools
 import pickle
-
-
-def load_VSWRM_data(path):
-    array2return = []
-    with open(path, 'rb') as f:
-        while True:
-            try:
-                array2return.append(pickle.load(f))
-            except EOFError:
-                break
-    return np.array(array2return)
+import json
 
 
 def plot_position(data_folder, robot_name, run_numbers, legends=None, suptitle=None, with_orientation=False,
@@ -35,7 +26,7 @@ def plot_position(data_folder, robot_name, run_numbers, legends=None, suptitle=N
         fig, ax = plt.subplots(3, 1, figsize=[10, 8])
 
     for run_number in run_numbers:
-        position_array = load_VSWRM_data(
+        position_array = data_tools.load_VSWRM_data(
             os.path.join(data_folder, robot_name, run_number, f'{robot_name}_run{run_number}_pos.npy'))
 
         fake_time = position_array[:, 0] / 1000
@@ -47,7 +38,7 @@ def plot_position(data_folder, robot_name, run_numbers, legends=None, suptitle=N
         plt.plot(fake_time, position_array[:, 3])
 
         if with_orientation:
-            or_array = load_VSWRM_data(
+            or_array = data_tools.load_VSWRM_data(
                 os.path.join(data_folder, robot_name, run_number, f'{robot_name}_run{run_number}_or.npy'))
             plt.axes(ax[2])
             plt.plot(fake_time, or_array[:, 1])
@@ -107,7 +98,7 @@ def plot_distance(data_folder, robot_names, run_number, legends=None, suptitle=N
     robot_positions = []
 
     for robot_name in robot_names:
-        position_array = load_VSWRM_data(
+        position_array = data_tools.load_VSWRM_data(
             os.path.join(data_folder, robot_name, run_number, f'{robot_name}_run{run_number}_pos.npy'))
         robot_positions.append(position_array)
 
@@ -138,5 +129,87 @@ def plot_distance(data_folder, robot_names, run_number, legends=None, suptitle=N
 
         fig, ax = plt.subplots(1, 1, figsize=[10, 8])
         plt.plot(distance_array)
+
+    plt.show()
+
+
+def plot_velocities(summary, data, changed_along=None, changed_along_alias=None):
+    velocities = data_tools.calculate_velocity(summary, data)
+    fig, ax = plt.subplots(summary['num_robots'], 1, figsize=[10, 8])
+
+    time = data[0, 0, 0, :-1]  # 1 step shorter because of diff
+
+    for i in range(summary['num_robots']):
+        if summary['num_robots'] > 1:
+            plt.axes(ax[i])
+
+        plt.plot(time, velocities[:, i, :].T)
+
+        if changed_along is not None:
+            if changed_along_alias is None:
+                legend = [f'{changed_along}={summary["params"][f"run{j+1}"][changed_along]}' for j in range(summary['num_runs'])]
+            else:
+                legend = [f'{changed_along_alias}={summary["params"][f"run{j + 1}"][changed_along]}' for j in
+                          range(summary['num_runs'])]
+            plt.legend(legend)
+
+        plt.title(f'Velocity of robot {i}')
+        plt.xlabel('simulation time [s]')
+        plt.ylabel('velocity [m/s]')
+
+    plt.show()
+
+
+def plot_distances(summary, data, reference_point, changed_along=None, changed_along_alias=None):
+    distances = data_tools.calculate_distance(summary, data, reference_point)
+    fig, ax = plt.subplots(summary['num_robots'], 1, figsize=[10, 8])
+
+    time = data[0, 0, 0, :]  # 1 step shorter because of diff
+
+    for i in range(summary['num_robots']):
+        if summary['num_robots'] > 1:
+            plt.axes(ax[i])
+        plt.plot(time, distances[:, i, :].T)
+
+        if changed_along is not None:
+            if changed_along_alias is None:
+                legend = [f'{changed_along}={summary["params"][f"run{j+1}"][changed_along]}' for j in range(summary['num_runs'])]
+            else:
+                legend = [f'{changed_along_alias}={summary["params"][f"run{j + 1}"][changed_along]}' for j in
+                          range(summary['num_runs'])]
+            plt.legend(legend)
+
+        plt.title(f'Distance of robot {i} from point {reference_point}')
+        plt.xlabel('simulation time [s]')
+        plt.ylabel('distance [m]')
+
+    plt.show()
+
+
+def plot_orientation(summary, data, changed_along=None, changed_along_alias=None):
+
+    fig, ax = plt.subplots(summary['num_robots'], 1, figsize=[10, 8])
+
+    time = data[0, 0, 0, :]  # 1 step shorter because of diff
+
+    or_idx = or_idx = summary['attributes'].index('or')
+    orientations = data[:, :, or_idx, :]
+
+    for i in range(summary['num_robots']):
+        if summary['num_robots'] > 1:
+            plt.axes(ax[i])
+        plt.plot(time, orientations[:, i, :].T)
+
+        if changed_along is not None:
+            if changed_along_alias is None:
+                legend = [f'{changed_along}={summary["params"][f"run{j+1}"][changed_along]}' for j in range(summary['num_runs'])]
+            else:
+                legend = [f'{changed_along_alias}={summary["params"][f"run{j + 1}"][changed_along]}' for j in
+                          range(summary['num_runs'])]
+            plt.legend(legend)
+
+        plt.title(f'Orientation of robot {i}')
+        plt.xlabel('simulation time [s]')
+        plt.ylabel('orientation [rad]')
 
     plt.show()
