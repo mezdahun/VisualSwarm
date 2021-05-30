ecc1bf42ee11347d8958573271ff784959dd2ad9
David <mezeydavid@gmail.com>
2021-05-30 13:03:04 +0200

    addded plotting

--- visualswarm/simulation_tools/plotting_tools.py
+++ visualswarm/simulation_tools/plotting_tools.py
@@ -207,3 +207,224 @@ def plot_min_iid(summary, data, changed_along=None, changed_along_alias=None):
         plt.ylabel('Min IID [m]')
 
     plt.show()
+
+def plot_mean_iid_over_runs(summary, data, stdcolor='#FF9848'):
+    iid = data_tools.calculate_interindividual_distance(summary, data)
+
+    population_mean = np.mean(np.mean(iid, 1), 1)
+    run_mean = np.mean(population_mean, 0)
+    run_std = np.std(population_mean, 0)
+    t = data[0, 0, 0, :]
+
+    for i in range(population_mean.shape[0]):
+        individ_line, = plt.plot(t, population_mean[i, :], ls="--", color="gray", linewidth="0.2")
+    mean_line, = plt.plot(t, run_mean, color="black", linewidth="1.2")
+    error_band = plt.fill_between(t, run_mean - run_std, run_mean + run_std, alpha=0.5, edgecolor=stdcolor,
+                                  facecolor=stdcolor)
+    plt.title("Mean inter-individual distance")
+    plt.xlabel("simulation time [s]")
+    plt.ylabel("I.I.D [m]$>$BL=0.11")
+    plt.legend([individ_line, mean_line, error_band], ["Single run population mean i.i.d.",
+                                                       "Mean population i.i.d. over runs",
+                                                       "Standard Deviation over runs"])
+    plt.show()
+
+
+def plot_mean_pol_over_runs(summary, data, stdcolor='#FF9848', ax=None, with_legend=None):
+    pol_m = data_tools.calculate_ploarization_matrix(summary, data)
+
+    population_mean = np.mean(np.mean(pol_m, 1), 1)
+    run_mean = np.mean(population_mean, 0)
+    run_std = np.std(population_mean, 0)
+    t = data[0, 0, 0, :]
+
+    if ax is not None:
+        plt.axes(ax)
+
+    for i in range(population_mean.shape[0]):
+        individ_line, = plt.plot(t, population_mean[i, :], ls="--", color="gray", linewidth="0.2")
+
+    mean_line, = plt.plot(t, run_mean, color="black", linewidth="1.2")
+    error_band = plt.fill_between(t, run_mean-run_std, run_mean+run_std, alpha=0.5, edgecolor=stdcolor, facecolor=stdcolor)
+
+    if ax is None:
+        plt.title("Population polarization (heading angle match)")
+        plt.xlabel("simulation time [s]")
+        plt.ylabel("polarization [AU]$\\in$[-1, 1]")
+        plt.legend([individ_line, mean_line, error_band], ["Single run population avg polarization",
+                                                           "Mean population polarization over runs",
+                                                           "Standard Deviation over runs"])
+        plt.show()
+    elif with_legend:
+        plt.legend([individ_line, mean_line, error_band], ["Single run population avg polarization",
+                                                           "Mean population polarization over runs",
+                                                           "Standard Deviation over runs"])
+
+def plot_mean_pol_summary_perInit(paths, titles, colors, supertitle,
+                                  xlabel="time [s]", ylabel="polariuzation [AU]$\\in$[0, 1]"):
+
+    fig, axs = plt.subplots(len(paths), 1, figsize=[10, 8], sharex=True, sharey=True)
+
+    for i in range(len(paths)):
+        summary, data = data_tools.read_summary_data(paths[i], os.path.basename(paths[i]))
+        plot_mean_pol_over_runs(summary, data, stdcolor=colors[i], ax=axs[i], with_legend=True)
+        plt.ylabel(ylabel)
+        plt.title(titles[i])
+
+    plt.xlabel(xlabel)
+    plt.suptitle(supertitle)
+    plt.show()
+
+def plot_mean_pol_summary_perRegimeandInit(regime_dict, titles, colors, supertitle,
+                                           xlabel="time [s]", ylabel="polariuzation [AU]$\\in$[0, 1]"):
+
+    fig, axs = plt.subplots(len(regime_dict), 1, figsize=[10, 8], sharex=True, sharey=True)
+
+    reg_i = 0
+    for regime_name, path_dict in regime_dict.items():
+        paths = path_dict['paths']
+        for i in range(len(paths)):
+            summary, data = data_tools.read_summary_data(paths[i], os.path.basename(paths[i]))
+            plot_mean_pol_over_runs(summary, data, stdcolor=colors[i], ax=axs[reg_i], with_legend=False)
+            plt.ylabel(ylabel)
+            plt.title(titles[reg_i])
+
+        reg_i += 1
+
+    plt.xlabel(xlabel)
+    plt.suptitle(supertitle)
+    plt.show()
+
+
+def plot_min_iid_over_runs(summary, data, stdcolor="#FF9848", ax=None, with_legend=None):
+    iid = data_tools.calculate_interindividual_distance(summary, data)
+    for i in range(summary['num_robots']):
+        iid[:, i, i, :] = np.inf
+
+    population_mean = np.mean(np.min(iid, 1), 1)
+    run_mean = np.mean(population_mean, 0)
+    run_std = np.std(population_mean, 0)
+    t = data[0, 0, 0, :]
+
+    if ax is not None:
+        plt.axes(ax)
+
+    for i in range(population_mean.shape[0]):
+        individ_line, = plt.plot(t, population_mean[i, :], ls="--", color="gray", linewidth="0.2")
+    mean_line, = plt.plot(t, run_mean, color="black", linewidth="1.2")
+    error_band = plt.fill_between(t, run_mean - run_std, run_mean + run_std, alpha=0.5, edgecolor=stdcolor,
+                                  facecolor=stdcolor)
+
+    if ax is None:
+        plt.title("AVG of Minimum inter-individual distances over runs")
+        plt.xlabel("simulation time [s]")
+        plt.ylabel("min I.I.D [m]$>$BL=0.11")
+        plt.legend([individ_line, mean_line, error_band], ["Min i.i.d. in single run",
+                                                           "Mean of minimum i.i.ds over runs",
+                                                           "Standard Deviation over runs"])
+        plt.show()
+    elif with_legend:
+        plt.legend([individ_line, mean_line, error_band], ["Min i.i.d. in single run",
+                                                           "Mean of minimum i.i.ds over runs",
+                                                           "Standard Deviation over runs"])
+
+def plot_min_iid_summary_perInit(paths, titles, colors, supertitle, xlabel="time [s]", ylabel="i-i.d. [m]"):
+
+    fig, axs = plt.subplots(len(paths), 1, figsize=[10, 8], sharex=True, sharey=True)
+
+    for i in range(len(paths)):
+        summary, data = data_tools.read_summary_data(paths[i], os.path.basename(paths[i]))
+        plot_min_iid_over_runs(summary, data, stdcolor=colors[i], ax=axs[i], with_legend=True)
+        plt.ylabel(ylabel)
+        plt.title(titles[i])
+
+    plt.xlabel(xlabel)
+    plt.suptitle(supertitle)
+    plt.show()
+
+def plot_min_iid_summary_perRegimeandInit(regime_dict, titles, colors, supertitle, xlabel="time [s]", ylabel="i-i d. [m]"):
+
+    fig, axs = plt.subplots(len(regime_dict), 1, figsize=[10, 8], sharex=True, sharey=True)
+
+    reg_i = 0
+    for regime_name, path_dict in regime_dict.items():
+        paths = path_dict['paths']
+        for i in range(len(paths)):
+            summary, data = data_tools.read_summary_data(paths[i], os.path.basename(paths[i]))
+            plot_min_iid_over_runs(summary, data, stdcolor=colors[i], ax=axs[reg_i], with_legend=False)
+            plt.ylabel(ylabel)
+            plt.title(titles[reg_i])
+
+        reg_i += 1
+
+    plt.xlabel(xlabel)
+    plt.suptitle(supertitle)
+    plt.show()
+
+
+def plot_mean_COMvel_over_runs(summary, data, stdcolor="#FF9848", ax=None, with_legend=None):
+    COMvelocity = data_tools.population_velocity(summary, data)
+
+    cut_beginning = 3
+
+    run_mean = np.mean(COMvelocity, 0)[cut_beginning:]
+    run_std = np.std(COMvelocity, 0)[cut_beginning:]
+
+    # cutting initial spike
+    t = data[0, 0, 0, cut_beginning:-1]
+
+    if ax is not None:
+        plt.axes(ax)
+
+    for i in range(COMvelocity.shape[0]):
+        individ_line, = plt.plot(t, COMvelocity[i, cut_beginning:], ls="--", color="gray", linewidth="0.2")
+    mean_line, = plt.plot(t, run_mean, color="black", linewidth="1.2")
+    error_band = plt.fill_between(t, run_mean - run_std, run_mean + run_std, alpha=0.5, edgecolor=stdcolor,
+                                  facecolor=stdcolor)
+
+    if ax is None:
+        plt.title("Mean absolute velocity of Center of Mass")
+        plt.xlabel("simulation time [s]")
+        plt.ylabel("mean v [m/s]")
+        plt.legend([individ_line, mean_line, error_band], ["Velocity of COM in individual runs",
+                                                           "Mean of COM velocity over runs",
+                                                           "Standard Deviation over runs"])
+
+        plt.show()
+    elif with_legend:
+        plt.legend([individ_line, mean_line, error_band], ["Velocity of COM in individual runs",
+                                                           "Mean of COM velocity over runs",
+                                                           "Standard Deviation over runs"])
+
+def plot_COMvelocity_summary_perInit(paths, titles, colors, supertitle, xlabel="time [s]", ylabel="v [m/s]"):
+
+    fig, axs = plt.subplots(len(paths), 1, figsize=[10, 8], sharex=True, sharey=True)
+
+    for i in range(len(paths)):
+        summary, data = data_tools.read_summary_data(paths[i], os.path.basename(paths[i]))
+        plot_mean_COMvel_over_runs(summary, data, stdcolor=colors[i], ax=axs[i], with_legend=True)
+        plt.ylabel(ylabel)
+        plt.title(titles[i])
+
+    plt.xlabel(xlabel)
+    plt.suptitle(supertitle)
+    plt.show()
+
+def plot_COMvelocity_summary_perRegimeandInit(regime_dict, titles, colors, supertitle, xlabel="time [s]", ylabel="v [m/s]"):
+
+    fig, axs = plt.subplots(len(regime_dict), 1, figsize=[10, 8], sharex=True, sharey=True)
+
+    reg_i = 0
+    for regime_name, path_dict in regime_dict.items():
+        paths = path_dict['paths']
+        for i in range(len(paths)):
+            summary, data = data_tools.read_summary_data(paths[i], os.path.basename(paths[i]))
+            plot_mean_COMvel_over_runs(summary, data, stdcolor=colors[i], ax=axs[reg_i], with_legend=False)
+            plt.ylabel(ylabel)
+            plt.title(titles[reg_i])
+
+        reg_i += 1
+
+    plt.xlabel(xlabel)
+    plt.suptitle(supertitle)
+    plt.show()
