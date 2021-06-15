01540edbafe9a9aafc7c17b0c106591be87f7dd2
mezdahun <mezeydavid@gmail.com>
2021-06-15 14:18:23 +0200

    added CNN vision for testing

--- visualswarm/vision/try_CNN_vision.py
+++ visualswarm/vision/try_CNN_vision.py
@@ -41,172 +41,182 @@ bcolors = logparams.BColors
 
 def CNN_vision():
 
-    picam = PiCamera()
-    picam.resolution = camera.RESOLUTION
-    picam.framerate = camera.FRAMERATE
-    logger.debug(f'\n{bcolors.OKBLUE}--Camera Params--{bcolors.ENDC}\n'
-                 f'{bcolors.OKBLUE}Resolution:{bcolors.ENDC} {camera.RESOLUTION} px\n'
-                 f'{bcolors.OKBLUE}Frame Rate:{bcolors.ENDC} {camera.FRAMERATE} fps')
+    try:
+        try:
 
-    MODEL_NAME = '/home/pi/VisualSwarm/data/tflite_model'
-    GRAPH_NAME = 'model.tflite'
-    LABELMAP_NAME = 'labelmap.txt'
-    min_conf_threshold = 0.85
+            picam = PiCamera()
+            picam.resolution = camera.RESOLUTION
+            picam.framerate = camera.FRAMERATE
+            logger.debug(f'\n{bcolors.OKBLUE}--Camera Params--{bcolors.ENDC}\n'
+                         f'{bcolors.OKBLUE}Resolution:{bcolors.ENDC} {camera.RESOLUTION} px\n'
+                         f'{bcolors.OKBLUE}Frame Rate:{bcolors.ENDC} {camera.FRAMERATE} fps')
 
-    resW, resH = camera.RESOLUTION
-    imW, imH = int(resW), int(resH)
-    use_TPU = False
+            MODEL_NAME = '/home/pi/VisualSwarm/data/tflite_model'
+            GRAPH_NAME = 'model.tflite'
+            LABELMAP_NAME = 'labelmap.txt'
+            min_conf_threshold = 0.85
 
-    # Generates a 3D RGB array and stores it in rawCapture
-    raw_capture = PiRGBArray(picam, size=camera.RESOLUTION)
+            resW, resH = camera.RESOLUTION
+            imW, imH = int(resW), int(resH)
+            use_TPU = False
 
-    # Wait a certain number of seconds to allow the camera time to warmup
-    time.sleep(0.1)
+            # Generates a 3D RGB array and stores it in rawCapture
+            raw_capture = PiRGBArray(picam, size=camera.RESOLUTION)
 
-    # Import TensorFlow libraries
-    # If tflite_runtime is installed, import interpreter from tflite_runtime, else import from regular tensorflow
-    # If using Coral Edge TPU, import the load_delegate library
-    # pkg = importlib.util.find_spec('tflite_runtime')
-    # if pkg:
-    #     from tflite_runtime.interpreter import Interpreter
-    #
-    #     if use_TPU:
-    #         from tflite_runtime.interpreter import load_delegate
-    # else:
-    from tensorflow.lite.python.interpreter import Interpreter
-    #
-    # if use_TPU:
-    #     from tensorflow.lite.python.interpreter import load_delegate
+            # Wait a certain number of seconds to allow the camera time to warmup
+            time.sleep(0.1)
 
-    # If using Edge TPU, assign filename for Edge TPU model
-    if use_TPU:
-        # If user has specified the name of the .tflite file, use that name, otherwise use default 'edgetpu.tflite'
-        if (GRAPH_NAME == 'detect.tflite'):
-            GRAPH_NAME = 'edgetpu.tflite'
+            # Import TensorFlow libraries
+            # If tflite_runtime is installed, import interpreter from tflite_runtime, else import from regular tensorflow
+            # If using Coral Edge TPU, import the load_delegate library
+            # pkg = importlib.util.find_spec('tflite_runtime')
+            # if pkg:
+            #     from tflite_runtime.interpreter import Interpreter
+            #
+            #     if use_TPU:
+            #         from tflite_runtime.interpreter import load_delegate
+            # else:
+            from tensorflow.lite.python.interpreter import Interpreter
+            #
+            # if use_TPU:
+            #     from tensorflow.lite.python.interpreter import load_delegate
 
-        # Get path to current working directory
-    CWD_PATH = os.getcwd()
+            # If using Edge TPU, assign filename for Edge TPU model
+            if use_TPU:
+                # If user has specified the name of the .tflite file, use that name, otherwise use default 'edgetpu.tflite'
+                if (GRAPH_NAME == 'detect.tflite'):
+                    GRAPH_NAME = 'edgetpu.tflite'
 
-    # Path to .tflite file, which contains the model that is used for object detection
-    PATH_TO_CKPT = os.path.join(CWD_PATH, MODEL_NAME, GRAPH_NAME)
+                # Get path to current working directory
+            CWD_PATH = os.getcwd()
 
-    # Path to label map file
-    PATH_TO_LABELS = os.path.join(CWD_PATH, MODEL_NAME, LABELMAP_NAME)
+            # Path to .tflite file, which contains the model that is used for object detection
+            PATH_TO_CKPT = os.path.join(CWD_PATH, MODEL_NAME, GRAPH_NAME)
 
-    # Load the label map
-    with open(PATH_TO_LABELS, 'r') as f:
-        labels = [line.strip() for line in f.readlines()]
+            # Path to label map file
+            PATH_TO_LABELS = os.path.join(CWD_PATH, MODEL_NAME, LABELMAP_NAME)
 
-    # Have to do a weird fix for label map if using the COCO "starter model" from
-    # https://www.tensorflow.org/lite/models/object_detection/overview
-    # First label is '???', which has to be removed.
-    if labels[0] == '???':
-        del (labels[0])
+            # Load the label map
+            with open(PATH_TO_LABELS, 'r') as f:
+                labels = [line.strip() for line in f.readlines()]
 
-    # Load the Tensorflow Lite model.
-    # If using Edge TPU, use special load_delegate argument
-    if use_TPU:
-        interpreter = Interpreter(model_path=PATH_TO_CKPT,
-                                  experimental_delegates=[load_delegate('libedgetpu.so.1.0')])
-        print(PATH_TO_CKPT)
-    else:
-        interpreter = Interpreter(model_path=PATH_TO_CKPT)
+            # Have to do a weird fix for label map if using the COCO "starter model" from
+            # https://www.tensorflow.org/lite/models/object_detection/overview
+            # First label is '???', which has to be removed.
+            if labels[0] == '???':
+                del (labels[0])
 
-    interpreter.allocate_tensors()
+            # Load the Tensorflow Lite model.
+            # If using Edge TPU, use special load_delegate argument
+            if use_TPU:
+                interpreter = Interpreter(model_path=PATH_TO_CKPT,
+                                          experimental_delegates=[load_delegate('libedgetpu.so.1.0')])
+                print(PATH_TO_CKPT)
+            else:
+                interpreter = Interpreter(model_path=PATH_TO_CKPT)
 
-    # Get model details
-    input_details = interpreter.get_input_details()
-    output_details = interpreter.get_output_details()
-    height = input_details[0]['shape'][1]
-    width = input_details[0]['shape'][2]
+            interpreter.allocate_tensors()
 
-    floating_model = (input_details[0]['dtype'] == np.float32)
+            # Get model details
+            input_details = interpreter.get_input_details()
+            output_details = interpreter.get_output_details()
+            height = input_details[0]['shape'][1]
+            width = input_details[0]['shape'][2]
 
-    input_mean = 127.5
-    input_std = 127.5
+            floating_model = (input_details[0]['dtype'] == np.float32)
 
-    # Initialize frame rate calculation
-    frame_rate_calc = 1
-    freq = cv2.getTickFrequency()
+            input_mean = 127.5
+            input_std = 127.5
+
+            # Initialize frame rate calculation
+            frame_rate_calc = 1
+            freq = cv2.getTickFrequency()
 
 
-    # Create window
-    cv2.namedWindow('Object detector', cv2.WINDOW_NORMAL)
+            # Create window
+            cv2.namedWindow('Object detector', cv2.WINDOW_NORMAL)
 
-    # for frame1 in camera.capture_continuous(rawCapture, format="bgr",use_video_port=True):
-    for image in picam.capture_continuous(raw_capture,
-                                          format=camera.CAPTURE_FORMAT,
-                                          use_video_port=camera.USE_VIDEO_PORT):
-        frame1 = image.array
+            # for frame1 in camera.capture_continuous(rawCapture, format="bgr",use_video_port=True):
+            for image in picam.capture_continuous(raw_capture,
+                                                  format=camera.CAPTURE_FORMAT,
+                                                  use_video_port=camera.USE_VIDEO_PORT):
+                frame1 = image.array
 
-        # Start timer (for calculating frame rate)
-        t1 = cv2.getTickCount()
+                # Start timer (for calculating frame rate)
+                t1 = cv2.getTickCount()
 
-        # Acquire frame and resize to expected shape [1xHxWx3]
-        frame = frame1.copy()
-        frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        frame_resized = cv2.resize(frame_rgb, (width, height))
-        input_data = np.expand_dims(frame_resized, axis=0)
+                # Acquire frame and resize to expected shape [1xHxWx3]
+                frame = frame1.copy()
+                frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+                frame_resized = cv2.resize(frame_rgb, (width, height))
+                input_data = np.expand_dims(frame_resized, axis=0)
 
-        # Normalize pixel values if using a floating model (i.e. if model is non-quantized)
-        if floating_model:
-            input_data = (np.float32(input_data) - input_mean) / input_std
+                # Normalize pixel values if using a floating model (i.e. if model is non-quantized)
+                if floating_model:
+                    input_data = (np.float32(input_data) - input_mean) / input_std
 
-        # Perform the actual detection by running the model with the image as input
-        interpreter.set_tensor(input_details[0]['index'], input_data)
-        interpreter.invoke()
+                # Perform the actual detection by running the model with the image as input
+                interpreter.set_tensor(input_details[0]['index'], input_data)
+                interpreter.invoke()
 
-        # Retrieve detection results
-        boxes = interpreter.get_tensor(output_details[0]['index'])[0]  # Bounding box coordinates of detected objects
-        classes = interpreter.get_tensor(output_details[1]['index'])[0]  # Class index of detected objects
-        scores = interpreter.get_tensor(output_details[2]['index'])[0]  # Confidence of detected objects
-        # num = interpreter.get_tensor(output_details[3]['index'])[0]  # Total number of detected objects (inaccurate and not needed)
+                # Retrieve detection results
+                boxes = interpreter.get_tensor(output_details[0]['index'])[0]  # Bounding box coordinates of detected objects
+                classes = interpreter.get_tensor(output_details[1]['index'])[0]  # Class index of detected objects
+                scores = interpreter.get_tensor(output_details[2]['index'])[0]  # Confidence of detected objects
+                # num = interpreter.get_tensor(output_details[3]['index'])[0]  # Total number of detected objects (inaccurate and not needed)
 
-        # Loop over all detections and draw detection box if confidence is above minimum threshold
-        for i in range(len(scores)):
-            if (scores[i] > min_conf_threshold) and (scores[i] <= 1.0):
-                # Get bounding box coordinates and draw box
-                # Interpreter can return coordinates that are outside of image dimensions, need to force them to be within image using max() and min()
-                ymin = int(max(1, (boxes[i][0] * imH)))
-                xmin = int(max(1, (boxes[i][1] * imW)))
-                ymax = int(min(imH, (boxes[i][2] * imH)))
-                xmax = int(min(imW, (boxes[i][3] * imW)))
+                # Loop over all detections and draw detection box if confidence is above minimum threshold
+                for i in range(len(scores)):
+                    if (scores[i] > min_conf_threshold) and (scores[i] <= 1.0):
+                        # Get bounding box coordinates and draw box
+                        # Interpreter can return coordinates that are outside of image dimensions, need to force them to be within image using max() and min()
+                        ymin = int(max(1, (boxes[i][0] * imH)))
+                        xmin = int(max(1, (boxes[i][1] * imW)))
+                        ymax = int(min(imH, (boxes[i][2] * imH)))
+                        xmax = int(min(imW, (boxes[i][3] * imW)))
 
-                cv2.rectangle(frame, (xmin, ymin), (xmax, ymax), (10, 255, 0), 2)
+                        cv2.rectangle(frame, (xmin, ymin), (xmax, ymax), (10, 255, 0), 2)
 
-                # Draw label
-                object_name = labels[int(classes[i])]  # Look up object name from "labels" array using class index
-                label = '%s: %d%%' % (object_name, int(scores[i] * 100))  # Example: 'person: 72%'
-                labelSize, baseLine = cv2.getTextSize(label, cv2.FONT_HERSHEY_SIMPLEX, 0.7, 2)  # Get font size
-                label_ymin = max(ymin, labelSize[1] + 10)  # Make sure not to draw label too close to top of window
-                cv2.rectangle(frame, (xmin, label_ymin - labelSize[1] - 10),
-                              (xmin + labelSize[0], label_ymin + baseLine - 10), (255, 255, 255),
-                              cv2.FILLED)  # Draw white box to put label text in
-                cv2.putText(frame, label, (xmin, label_ymin - 7), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 0),
-                            2)  # Draw label text
+                        # Draw label
+                        object_name = labels[int(classes[i])]  # Look up object name from "labels" array using class index
+                        label = '%s: %d%%' % (object_name, int(scores[i] * 100))  # Example: 'person: 72%'
+                        labelSize, baseLine = cv2.getTextSize(label, cv2.FONT_HERSHEY_SIMPLEX, 0.7, 2)  # Get font size
+                        label_ymin = max(ymin, labelSize[1] + 10)  # Make sure not to draw label too close to top of window
+                        cv2.rectangle(frame, (xmin, label_ymin - labelSize[1] - 10),
+                                      (xmin + labelSize[0], label_ymin + baseLine - 10), (255, 255, 255),
+                                      cv2.FILLED)  # Draw white box to put label text in
+                        cv2.putText(frame, label, (xmin, label_ymin - 7), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 0),
+                                    2)  # Draw label text
 
-                # Draw circle in center
-                xcenter = xmin + (int(round((xmax - xmin) / 2)))
-                ycenter = ymin + (int(round((ymax - ymin) / 2)))
-                cv2.circle(frame, (xcenter, ycenter), 5, (0, 0, 255), thickness=-1)
+                        # Draw circle in center
+                        xcenter = xmin + (int(round((xmax - xmin) / 2)))
+                        ycenter = ymin + (int(round((ymax - ymin) / 2)))
+                        cv2.circle(frame, (xcenter, ycenter), 5, (0, 0, 255), thickness=-1)
 
-                # Print info
-                print('Object ' + str(i) + ': ' + object_name + ' at (' + str(xcenter) + ', ' + str(ycenter) + ')')
+                        # Print info
+                        print('Object ' + str(i) + ': ' + object_name + ' at (' + str(xcenter) + ', ' + str(ycenter) + ')')
 
-        # Draw framerate in corner of frame
-        cv2.putText(frame, 'FPS: {0:.2f}'.format(frame_rate_calc), (30, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 0), 2,
-                    cv2.LINE_AA)
+                # Draw framerate in corner of frame
+                cv2.putText(frame, 'FPS: {0:.2f}'.format(frame_rate_calc), (30, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 0), 2,
+                            cv2.LINE_AA)
 
-        # All the results have been drawn on the frame, so it's time to display it.
-        cv2.imshow('Object detector', frame)
+                # All the results have been drawn on the frame, so it's time to display it.
+                cv2.imshow('Object detector', frame)
 
-        # Calculate framerate
-        t2 = cv2.getTickCount()
-        time1 = (t2 - t1) / freq
-        frame_rate_calc = 1 / time1
+                # Calculate framerate
+                t2 = cv2.getTickCount()
+                time1 = (t2 - t1) / freq
+                frame_rate_calc = 1 / time1
 
-        raw_capture.truncate(0)
+                raw_capture.truncate(0)
 
-        # Press 'q' to quit
-        if cv2.waitKey(1) == ord('q'):
-            break
+                # Press 'q' to quit
+                if cv2.waitKey(1) == ord('q'):
+                    break
+        except KeyboardInterrupt:
+            try:
+                pass
+            except PiCameraValueError:
+                pass
+    except PiCameraValueError:
+        pass
