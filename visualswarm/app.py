87e9df9a4420149d14d6f8786339216cba7061b9
mezdahun <mezeydavid@gmail.com>
2021-06-15 13:10:20 +0200

    added CNN vision for testing

--- visualswarm/app.py
+++ visualswarm/app.py
@@ -199,3 +199,14 @@ def start_application(with_control=False):
 
 def start_application_with_control():
     start_application(with_control=True)
+
+def start_CNN_vision():
+    try:
+        raw_vision = Process(target=vacquire.raw_vision, args=(raw_vision_stream,))
+        raw_vision.start()
+        raw_vision.join()
+    except KeyboardInterrupt:
+        logger.info(f'{bcolors.WARNING}EXIT gracefully on KeyboardInterrupt{bcolors.ENDC}')
+        # Terminating Processes
+        raw_vision.terminate()
+        raw_vision.join()
