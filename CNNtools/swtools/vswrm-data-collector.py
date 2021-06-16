4011cf8e595cab64e4b8949177a0040a9532d808
David <mezeydavid@gmail.com>
2021-06-16 09:49:46 +0200

    added help

--- CNNtools/swtools/vswrm-data-collector.py
+++ CNNtools/swtools/vswrm-data-collector.py
@@ -34,8 +34,10 @@ HELP_MESSAGE = """
     
     The SW collects images in JPG format into a given directory (savedir). 
     The framerate (framerate) and resolution of the camera (resolution) can
-    be changed via flags. After starting the software hit Space to capture image or
-    Esc to quit.
+    be changed via flags. After starting the software hit 's' to capture image or
+    'q' to quit.
+    
+    IMPORTANT: The focus must be on the video window to capture key press!
 
 """
 
@@ -62,64 +64,65 @@ os.makedirs(SAVE_FOLDER, exist_ok=True)
 RESOLUTION = [int(i) for i in args.resolution.split('x')]
 FRAMERATE = int(args.framerate)
 
-# try:
-#     try:
-picam = PiCamera()
-picam.resolution = RESOLUTION
-picam.framerate = FRAMERATE
-logger.info(f'\n\t{bcolors.OKBLUE}--Camera Params--{bcolors.ENDC}\n'
-            f'\t{bcolors.OKBLUE}Resolution:{bcolors.ENDC} {RESOLUTION} px\n'
-            f'\t{bcolors.OKBLUE}Frame Rate:{bcolors.ENDC} {FRAMERATE} fps\n'
-            f'\n\t-- images are saved in {SAVE_FOLDER}\n'
-            f'\t-- press {bcolors.OKBLUE}Space{bcolors.ENDC} to save frame.\n'
-            f'\t-- press {bcolors.FAIL}Esc{bcolors.ENDC} or {bcolors.FAIL}Ctrl+C{bcolors.ENDC} to quit.\n')
+try:
+    try:
+        picam = PiCamera()
+        picam.resolution = RESOLUTION
+        picam.framerate = FRAMERATE
+        logger.info(f'\n\t{bcolors.OKBLUE}--Camera Params--{bcolors.ENDC}\n'
+                    f'\t{bcolors.OKBLUE}Resolution:{bcolors.ENDC} {RESOLUTION} px\n'
+                    f'\t{bcolors.OKBLUE}Frame Rate:{bcolors.ENDC} {FRAMERATE} fps\n'
+                    f'\n\t-- images are saved in {SAVE_FOLDER}\n'
+                    f'\t-- press {bcolors.OKBLUE}s key{bcolors.ENDC} to save frame.\n'
+                    f'\t-- press {bcolors.FAIL}q key{bcolors.ENDC} or {bcolors.FAIL}Ctrl+C{bcolors.ENDC} to quit.\n'
+                    f'\t-- {bcolors.WARNING}IMPORTANT:{bcolors.ENDC} keep the focus on the video window to catch key strokes!')
 
-# Generates a 3D RGB array and stores it in rawCapture
-raw_capture = PiRGBArray(picam, size=RESOLUTION)
+        # Generates a 3D RGB array and stores it in rawCapture
+        raw_capture = PiRGBArray(picam, size=RESOLUTION)
 
-# Wait a certain number of seconds to allow the camera time to warmup
-logger.info('--Waiting 8 seconds for PI-camera to warmup!')
-time.sleep(8)
-logger.info('--Start Video Stream')
-
-
-cv2.namedWindow('Camera Stream')
-
-frame_id = 0
-for frame in picam.capture_continuous(raw_capture,
-                                      format='bgr',
-                                      use_video_port=True):
-    # Grab the raw NumPy array representing the image
-    image = frame.array
-
-    cv2.imshow('Camera Stream', image)
-    k = cv2.waitKey(1) & 0xFF
-    raw_capture.truncate(0)
-
-    logger.info(k)
-
-    if k == ord("q"):
-        # ESC pressed
-        logger.info("Escape was hit, closing...")
-        break
-    elif k == ord("s"):
-        # SPACE pressed
-        image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-        img_name = os.path.join(SAVE_FOLDER, f"opencv_frame_{frame_id}.jpg")
-        cv2.imwrite(img_name, image)
-        logger.info(f"{img_name} saved!")
+        # Wait a certain number of seconds to allow the camera time to warmup
+        logger.info('--Waiting 8 seconds for PI-camera to warmup!')
+        time.sleep(8)
+        logger.info('--Start Video Stream')
 
 
-    frame_id += 1
+        cv2.namedWindow('Camera Stream')
 
-f'-- {bcolors.OKBLUE}Bye Bye!{bcolors.ENDC}'
-#     except KeyboardInterrupt:
-#         try:
-#             f'-- {bcolors.FAIL}KeyboardInterrupt!{bcolors.ENDC} Exiting gracefully...'
-#             f'-- {bcolors.OKBLUE}Bye Bye!{bcolors.ENDC}'
-#             pass
-#         except PiCameraValueError:
-#             pass
-# except PiCameraValueError:
-#     logger.warning(f'-- {bcolors.FAIL}PiCameraError detected!{bcolors.ENDC}')
-#     pass
+        frame_id = 0
+        for frame in picam.capture_continuous(raw_capture,
+                                              format='bgr',
+                                              use_video_port=True):
+            # Grab the raw NumPy array representing the image
+            image = frame.array
+
+            cv2.imshow('Camera Stream', image)
+            k = cv2.waitKey(1) & 0xFF
+            raw_capture.truncate(0)
+
+            logger.info(k)
+
+            if k == ord("q"):
+                # ESC pressed
+                logger.info("'q' was hit, closing...")
+                break
+            elif k == ord("s"):
+                # s pressed
+                image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+                img_name = os.path.join(SAVE_FOLDER, f"opencv_frame_{frame_id}.jpg")
+                cv2.imwrite(img_name, image)
+                logger.info(f"{img_name} saved!")
+
+
+            frame_id += 1
+
+        f'-- {bcolors.OKBLUE}Bye Bye!{bcolors.ENDC}'
+    except KeyboardInterrupt:
+        try:
+            f'-- {bcolors.FAIL}KeyboardInterrupt!{bcolors.ENDC} Exiting gracefully...'
+            f'-- {bcolors.OKBLUE}Bye Bye!{bcolors.ENDC}'
+            pass
+        except PiCameraValueError:
+            pass
+except PiCameraValueError:
+    logger.warning(f'-- {bcolors.FAIL}PiCameraError detected!{bcolors.ENDC}')
+    pass
