6e826999f98cc2676aa1c1dbd8de307c1df460c9
mezdahun <mezeydavid@gmail.com>
2021-06-18 09:15:44 +0200

    started to work on datacollector

--- visualswarm/vision/vprocess.py
+++ visualswarm/vision/vprocess.py
@@ -184,16 +184,16 @@ def high_level_vision(raw_vision_stream, high_level_vision_stream, visualization
 
                 for i in range(len(scores)):
                     #if (scores[i] > min_conf_threshold) and (scores[i] <= 1.0):
-                    if scores[i] == np.max(scores):
+                    # if scores[i] == np.max(scores):
                     # Get bounding box coordinates and draw box
                     # Interpreter can return coordinates that are outside of image dimensions, need to force them to be within image using max() and min()
-                        ymin = int(max(1, (boxes[i][0] * imH)))
-                        xmin = int(max(1, (boxes[i][1] * imW)))
-                        ymax = int(min(imH, (boxes[i][2] * imH)))
-                        xmax = int(min(imW, (boxes[i][3] * imW)))
+                    ymin = int(max(1, (boxes[i][0] * imH)))
+                    xmin = int(max(1, (boxes[i][1] * imW)))
+                    ymax = int(min(imH, (boxes[i][2] * imH)))
+                    xmax = int(min(imW, (boxes[i][3] * imW)))
 
-                        cv2.rectangle(blurred, (xmin, ymin), (xmax, ymax), (10, 255, 0), 2)
-                        logger.info(f'Detection @ {(xmin, ymin)} with score {scores[i]}')
+                    cv2.rectangle(blurred, (xmin, ymin), (xmax, ymax), (10, 255, 0), 2)
+                    logger.info(f'Detection @ {(xmin, ymin)} with score {scores[i]}')
 
             # Forwarding result to VPF extraction
             logger.info(f'queue {raw_vision_stream.qsize()}')
