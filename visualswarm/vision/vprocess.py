6cc29c6e168c029ca246fb9e584cc553206e14f3
mezdahun <mezeydavid@gmail.com>
2021-06-18 17:33:18 +0200

    started to work on datacollector

--- visualswarm/vision/vprocess.py
+++ visualswarm/vision/vprocess.py
@@ -75,7 +75,7 @@ def high_level_vision(raw_vision_stream, high_level_vision_stream, visualization
 
             logger.info('Loading tensorflow model...')
             MODEL_NAME = '/home/pi/VisualSwarm/CNNtools/data/tflite_model/edgetpu'
-            GRAPH_NAME = 'model_quant.tflite'
+            GRAPH_NAME = 'model_quant_full_integer_edgetpu.tflite'
             LABELMAP_NAME = 'labelmap.txt'
             USE_TPU = True
 
