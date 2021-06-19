1a8e7cfce0aca2477f6d45a9c5c3c3bad5a68d90
mezdahun <mezeydavid@gmail.com>
2021-06-19 12:11:07 +0200

    started to work on datacollector

--- visualswarm/contrib/camera.py
+++ visualswarm/contrib/camera.py
@@ -3,7 +3,7 @@
 @description: Camera module related parameters
 """
 # Basic parameters
-RESOLUTION = (800, 600)
+RESOLUTION = (480, 360)
 FRAMERATE = 15
 CAPTURE_FORMAT = "bgr"
 USE_VIDEO_PORT = True
