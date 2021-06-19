c649b5a710d2fa9c9844e1f65b5bc962d7879f81
mezdahun <mezeydavid@gmail.com>
2021-06-19 13:56:28 +0200

    pycoral is a lie

--- visualswarm/vision/vprocess.py
+++ visualswarm/vision/vprocess.py
@@ -334,10 +334,10 @@ def high_level_vision_(raw_vision_stream, high_level_vision_stream, visualizatio
 
     logger.info('Loading tensorflow model...')
     MODEL_NAME = '/home/pi/VisualSwarm/CNNtools/data/tflite_model/edgetpu'
-    GRAPH_NAME = 'model_test_2_edgetpu.tflite'
+    GRAPH_NAME = 'model_test_fullinteger_edgetpu.tflite'
     LABELMAP_NAME = 'labelmap.txt'
     USE_TPU = True
-    INTQUANT = False
+    INTQUANT = True
     # it takes a little longer on the first run and then runs at normal speed.
     import random
     import glob
