2282d5e1d9bb6efcd01d63e386f5bb7e48a86845
mezdahun <mezeydavid@gmail.com>
2021-06-20 14:55:22 +0200

    resizing from large resolution to keep FOV

--- CNNtools/swtools/vswrm-data-collector.py
+++ CNNtools/swtools/vswrm-data-collector.py
@@ -78,7 +78,7 @@ try:
                     f'\t-- {bcolors.WARNING}IMPORTANT:{bcolors.ENDC} keep the focus on the video window to catch key strokes!')
 
         # Generates a 3D RGB array and stores it in rawCapture
-        raw_capture = PiRGBArray(picam, size=RESOLUTION)
+        raw_capture = PiRGBArray(picam, size=[int(1280), int(720)])#RESOLUTION)
 
         # Wait a certain number of seconds to allow the camera time to warmup
         logger.info('--Waiting 8 seconds for PI-camera to warmup!')
@@ -94,7 +94,7 @@ try:
                                               use_video_port=True):
             # Grab the raw NumPy array representing the image
             image = cv2.flip(frame.array, -1)
-            # image = cv2.resize(image, RESOLUTION)
+            image = cv2.resize(image, RESOLUTION)
 
             cv2.imshow('Camera Stream', image)
             k = cv2.waitKey(1) & 0xFF
