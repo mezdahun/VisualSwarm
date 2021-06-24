80e2c445bc93ff3d8c147378a2a9efb1aaf653a3
mezdahun <mezeydavid@gmail.com>
2021-06-24 09:52:52 +0200

    added network trained on fisheye lense

--- visualswarm/contrib/camera.py
+++ visualswarm/contrib/camera.py
@@ -3,7 +3,7 @@
 @description: Camera module related parameters
 """
 # Basic parameters
-RESOLUTION = (1280, 720)
+RESOLUTION = (320, 200)
 FRAMERATE = 20
 CAPTURE_FORMAT = "bgr"
 USE_VIDEO_PORT = True
