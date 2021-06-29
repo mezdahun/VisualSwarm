bff8b6392376c374d8156a75c3d3a6daa0e1e542
mezdahun <mezeydavid@gmail.com>
2021-06-29 12:41:01 +0200

    fixed tests before functionality testing

--- visualswarm/app_puppetmaster.py
+++ visualswarm/app_puppetmaster.py
@@ -8,19 +8,23 @@ import logging
 # # setup logging
 # logging.basicConfig()
 logger = logging.getLogger(__name__)
-logger.setLevel('ERROR')
+logger.setLevel('INFO')
 
 
 def start_swarm():
     logger.info('Puppetmaster started!')
-    # with Connection(puppetmaster.HOSTS[0], user=puppetmaster.UNAME) as c:
-    #     c.connect_kwargs.password = puppetmaster.PSWD
-    #     c.run('cd Desktop/VisualSwarm && dtach -n /tmp/tmpdtach pipenv run vswrm-start-vision', hide=True, pty=False)
-    #     start_result = c.run('ps ax  | grep "dtach -n /tmp/tmpdtach pipenv run vswrm-start-vision"')
-    #     PID = start_result.stdout.split()[0]
-    #     print(f'Started process with PID: {int(PID)}')
-    #     print('going to sleeeeeep...')
-    #     sleep(5)
-    #     print('Waking up and killing process!')
-    #     end_result = c.run(f'kill -INT {int(PID)}')
-    #     print(end_result.stdout)
+    with Connection(puppetmaster.HOSTS[0], user=puppetmaster.UNAME) as c:
+        c.connect_kwargs.password = puppetmaster.PSWD
+        c.run('cd Desktop/VisualSwarm && '
+              'dtach -n /tmp/tmpdtach '
+              'pipenv run vswrm-start-vision',
+              hide=True,
+              pty=False)
+        start_result = c.run('ps ax  | grep "dtach -n /tmp/tmpdtach pipenv run vswrm-start-vision"')
+        PID = start_result.stdout.split()[0]
+        print(f'Started process with PID: {int(PID)}')
+        print('going to sleeeeeep...')
+        sleep(5)
+        print('Waking up and killing process!')
+        end_result = c.run(f'kill -INT {int(PID)}')
+        print(end_result.stdout)
