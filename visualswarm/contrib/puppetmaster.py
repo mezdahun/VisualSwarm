efb46adf1b39683963a79708c6d3fac887b04088
David <mezeydavid@gmail.com>
2021-07-05 11:28:04 +0200

    added fixes to puppetmaster

--- visualswarm/contrib/puppetmaster.py
+++ visualswarm/contrib/puppetmaster.py
@@ -3,6 +3,7 @@
 @description: Parameters related to the puppetmaster submodule controlling a swarm of robots via SSH with fabric
 """
 
-HOSTS = {'Robot1': '130.149.244.192'}
+HOSTS = {'Robot1': '192.168.100.162',
+         'Robot2': '192.168.100.179'}
 UNAME = 'pi'
 INSTALL_DIR = '/home/pi/VisualSwarm'
