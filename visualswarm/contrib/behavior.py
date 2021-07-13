77de368f563ef3d7b790d07a44fc90158d6dff14
mezdahun <mezeydavid@gmail.com>
2021-07-13 15:34:55 +0200

    measure eq dist

--- visualswarm/contrib/behavior.py
+++ visualswarm/contrib/behavior.py
@@ -21,7 +21,7 @@ else:
 
 # Velocity Parameters
 GAM = behave_params_dict.get('GAM', 0.55)
-V0 = behave_params_dict.get('V0', 20)
+V0 = behave_params_dict.get('V0', 75)
 ALP0 = behave_params_dict.get('ALP0', 10000)
 ALP1 = behave_params_dict.get('ALP1', 0.3)
 ALP2 = behave_params_dict.get('ALP2', 0)
