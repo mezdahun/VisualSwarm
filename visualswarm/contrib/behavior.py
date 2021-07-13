20529fa8e2327d6046d82a0195d8783cd57a4c1e
mezdahun <mezeydavid@gmail.com>
2021-07-13 11:38:57 +0200

    fix app puppet

--- visualswarm/contrib/behavior.py
+++ visualswarm/contrib/behavior.py
@@ -21,7 +21,7 @@ else:
 
 # Velocity Parameters
 GAM = behave_params_dict.get('GAM', 0.1)
-V0 = behave_params_dict.get('V0', 0)
+V0 = behave_params_dict.get('V0', 50)
 ALP0 = behave_params_dict.get('ALP0', 125)
 ALP1 = behave_params_dict.get('ALP1', 0.00005)
 ALP2 = behave_params_dict.get('ALP2', 0)
