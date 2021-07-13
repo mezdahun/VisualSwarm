ed18a3dec269ea6ac6f071183cd38327b48d9839
mezdahun <mezeydavid@gmail.com>
2021-07-13 11:58:27 +0200

    fix app puppet

--- visualswarm/contrib/behavior.py
+++ visualswarm/contrib/behavior.py
@@ -20,15 +20,15 @@ else:
     behave_params_dict = {}
 
 # Velocity Parameters
-GAM = behave_params_dict.get('GAM', 0.3)
+GAM = behave_params_dict.get('GAM', 0.2)
 V0 = behave_params_dict.get('V0', 0)
 ALP0 = behave_params_dict.get('ALP0', 200)
-ALP1 = behave_params_dict.get('ALP1', 0.0005)
+ALP1 = behave_params_dict.get('ALP1', 0.005)
 ALP2 = behave_params_dict.get('ALP2', 0)
 
 # Heading Vector Parameters
-BET0 = behave_params_dict.get('BET0', 0)
-BET1 = behave_params_dict.get('BET1', 0.0005)
+BET0 = behave_params_dict.get('BET0', 0.5)
+BET1 = behave_params_dict.get('BET1', 0.005)
 BET2 = behave_params_dict.get('BET2', 0)
 
 # Motor scale heuristics Kappa
