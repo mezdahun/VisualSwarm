bbe3be20ffe82b3b3c5069bc88610e47a5ec19a3
mezdahun <mezeydavid@gmail.com>
2021-07-14 09:52:38 +0200

    tuning parameters

--- visualswarm/contrib/behavior.py
+++ visualswarm/contrib/behavior.py
@@ -23,12 +23,12 @@ else:
 GAM = behave_params_dict.get('GAM', 0.55)
 V0 = behave_params_dict.get('V0', 20)
 ALP0 = behave_params_dict.get('ALP0', 500000)
-ALP1 = behave_params_dict.get('ALP1', 0.7)
+ALP1 = behave_params_dict.get('ALP1', 0.2)
 ALP2 = behave_params_dict.get('ALP2', 0)
 
 # Heading Vector Parameters
-BET0 = behave_params_dict.get('BET0', 100)
-BET1 = behave_params_dict.get('BET1', 5)
+BET0 = behave_params_dict.get('BET0', 350)
+BET1 = behave_params_dict.get('BET1', 0.3)
 BET2 = behave_params_dict.get('BET2', 0)
 
 # Motor scale heuristics Kappa
