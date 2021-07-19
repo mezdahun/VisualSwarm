2a0bd2ade8a2ae24ce218385047bf562de33fb82
mezdahun <mezeydavid@gmail.com>
2021-07-19 13:17:12 +0200

    no

--- visualswarm/contrib/behavior.py
+++ visualswarm/contrib/behavior.py
@@ -27,8 +27,8 @@ ALP1 = behave_params_dict.get('ALP1', 0.00075)
 ALP2 = behave_params_dict.get('ALP2', 0)
 
 # Heading Vector Parameters
-BET0 = behave_params_dict.get('BET0', 0.75)
-BET1 = behave_params_dict.get('BET1', 0.5)
+BET0 = behave_params_dict.get('BET0', 0.5)
+BET1 = behave_params_dict.get('BET1', 0.1)
 BET2 = behave_params_dict.get('BET2', 0)
 
 # Motor scale heuristics Kappa
