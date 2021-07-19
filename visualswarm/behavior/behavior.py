4c147370a8f5256aae89bf10f51f3ddb2e4b4e81
mezdahun <mezeydavid@gmail.com>
2021-07-19 17:12:26 +0200

    pass behave parameters as individual env vars

--- visualswarm/behavior/behavior.py
+++ visualswarm/behavior/behavior.py
@@ -73,7 +73,7 @@ def VPF_to_behavior(VPF_stream, control_stream, motor_control_mode_stream, with_
             v += dv * dt
 
             logger.warning(f'dV={np.abs(dv * dt)} with passed seconds {(t_now - start_behave).total_seconds()}')
-            if np.abs(dv * dt) < 0.15 and (t_now - start_behave).total_seconds() > 10:
+            if np.abs(dv * dt) < 0.15 and (t_now - start_behave).total_seconds() > 15:
                 logger.warning('STOP EXPERIMENT!!!!')
                 raise KeyboardInterrupt('DV decreased to zero and already 5 sec gone from experiment!!!')
 
