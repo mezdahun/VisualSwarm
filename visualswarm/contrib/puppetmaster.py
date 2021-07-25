e7ca19cd992c8f663f99760a1b4849cea40c246c
mezdahun <mezeydavid@gmail.com>
2021-07-25 10:57:36 +0200

    Added robot IPs

--- visualswarm/contrib/puppetmaster.py
+++ visualswarm/contrib/puppetmaster.py
@@ -4,6 +4,8 @@
 """
 
 HOSTS = {'Robot1': '192.168.100.162',
-         'Robot2': '192.168.100.179'}
+         'Robot2': '192.168.100.179',
+         'Robot3': '192.168.100.131',
+         'Robot4': '192.168.100.170'}
 UNAME = 'pi'
 INSTALL_DIR = '/home/pi/VisualSwarm'
