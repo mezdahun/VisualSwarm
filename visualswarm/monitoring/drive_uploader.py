1ce6da46cbde5b21f7405a769d2aab72511c12a5
mezdahun <mezeydavid@gmail.com>
2021-07-27 12:33:22 +0200

    fixed zip path

--- visualswarm/monitoring/drive_uploader.py
+++ visualswarm/monitoring/drive_uploader.py
@@ -114,7 +114,7 @@ def upload_vision_videos(videos_folder=None):
                 if monitoring.CLOUD_STORAGE_AUTH_MODE == 'ServiceAccount':
                     cloudPermissions = drive_service.permissions().create(fileId=new_file['id'],
                                                                           body={'type': 'user',
-                                                                                'role': 'reader',
+                                                                                'role': 'owner',
                                                                                  'emailAddress': 'visualswarm.scioi@gmail.com'}).execute()
 
                 logger.info(f"\nFile created, id@drive: {new_file.get('id')}, local file: {os.path.split(filename)[1]}")
@@ -162,7 +162,7 @@ def upload_statevars(videos_folder=None):
                 if monitoring.CLOUD_STORAGE_AUTH_MODE == 'ServiceAccount':
                     cloudPermissions = drive_service.permissions().create(fileId=new_file['id'],
                                                                           body={'type': 'user',
-                                                                                'role': 'reader',
+                                                                                'role': 'owner',
                                                                                  'emailAddress': 'visualswarm.scioi@gmail.com'}).execute()
 
                 logger.info(f"\nFile created, id@drive: {new_file.get('id')}, local file: {os.path.split(filename)[1]}")
@@ -218,7 +218,7 @@ def zipupload_CNN_training_data(training_data_folder=None):
     if monitoring.CLOUD_STORAGE_AUTH_MODE == 'ServiceAccount':
         cloudPermissions = drive_service.permissions().create(fileId=new_file['id'],
                                                               body={'type': 'user',
-                                                                    'role': 'reader',
+                                                                    'role': 'owner',
                                                                     'emailAddress': 'visualswarm.scioi@gmail.com'}).execute()
 
     logger.info(f"\nFile created, id@drive: {new_file.get('id')}, local file: {zip_filename}")
