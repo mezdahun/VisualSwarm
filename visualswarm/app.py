a7d020e8a7f34afb5456a75df1519414b03fa3cb
mezdahun <mezeydavid@gmail.com>
2021-07-27 13:29:29 +0200

    parametrized folder id

--- visualswarm/app.py
+++ visualswarm/app.py
@@ -205,8 +205,9 @@ def start_application(with_control=False):
             logger.info(f'{bcolors.OKGREEN}UPLOAD{bcolors.ENDC} generated videos to Google Drive...')
             drive_uploader.upload_vision_videos(monitoring.SAVED_VIDEO_FOLDER)
             drive_uploader.upload_statevars(monitoring.SAVED_VIDEO_FOLDER)
-            training_data_folder = os.path.join(monitoring.SAVED_VIDEO_FOLDER, 'training_data')
-            drive_uploader.zipupload_CNN_training_data(training_data_folder)
+            if monitoring.SAVE_CNN_TRAINING_DATA:
+                training_data_folder = os.path.join(monitoring.SAVED_VIDEO_FOLDER, 'training_data')
+                drive_uploader.zipupload_CNN_training_data(training_data_folder)
 
         logger.info(f'{bcolors.OKGREEN}EXITED Gracefully. Bye bye!{bcolors.ENDC}')
 
