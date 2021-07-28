a8f59a06f8538ed5709db821d34f705626ad8c5a
mezdahun <mezeydavid@gmail.com>
2021-07-28 13:36:24 +0200

    including 3rd robot

--- visualswarm/contrib/puppetmaster.py
+++ visualswarm/contrib/puppetmaster.py
@@ -1,6 +1,9 @@
 """
 @author: mezdahun
 @description: Parameters related to the puppetmaster submodule controlling a swarm of robots via SSH with fabric
+
+example command on robot: ENABLE_CLOUD_LOGGING=0 ENABLE_CLOUD_STORAGE=0 SAVE_VISION_VIDEO=1 SHOW_VISION_STREAMS=1
+FLIP_CAMERA=0 ROBOT_FOV=3.8 LOG_LEVEL=DEBUG SAVE_CNN_TRAINING_DATA=0 ROBOT_NAME=Robot2 vswrm-start-vision
 """
 
 HOSTS = {'Robot1': '192.168.100.162',
