256cb446620e9a6b7ab65562fb0edddc2aa30e4c
mezdahun <mezeydavid@gmail.com>
2021-07-28 09:37:35 +0200

    added lens configuration to contrib.vision

--- visualswarm/contrib/vision.py
+++ visualswarm/contrib/vision.py
@@ -3,8 +3,17 @@
 @description: Parameters related to visualization
 """
 from cv2 import cvtColor, COLOR_BGR2HSV
-from numpy import uint8
+import numpy as np
 import os
+
+def calculate_reverse_mapping_fn(lens, orig_img_width):
+    # calculate discretized reverse mapping for fisheye correction according to lens parameters
+    lens['h_reverse_mapping'] = np.array(
+        [np.round(num) for num in np.maximum(lens['a_nonlin'] * np.square(lens['h_domain_orig']),
+                                             lens['a_lin'] * np.ones(orig_img_width) - lens['offset_lin'])])
+    lens['new_width'] = np.sum(lens['h_reverse_mapping'])
+    return lens
+
 
 # Recognition Type, supported: 'Color' or 'CNN'
 RECOGNITION_TYPE = "CNN"
@@ -36,9 +45,9 @@ HSV_HUE_RANGE = 29
 SV_MINIMUM = 112
 SV_MAXIMUM = 255
 R, G, B = TARGET_RGB_COLOR
-TARGET_HSV_COLOR = cvtColor(uint8([[[B, G, R]]]), COLOR_BGR2HSV)
-HSV_LOW = uint8([TARGET_HSV_COLOR[0][0][0] - HSV_HUE_RANGE, SV_MINIMUM, SV_MINIMUM])
-HSV_HIGH = uint8([TARGET_HSV_COLOR[0][0][0] + HSV_HUE_RANGE, SV_MAXIMUM, SV_MAXIMUM])
+TARGET_HSV_COLOR = cvtColor(np.uint8([[[B, G, R]]]), COLOR_BGR2HSV)
+HSV_LOW = np.uint8([TARGET_HSV_COLOR[0][0][0] - HSV_HUE_RANGE, SV_MINIMUM, SV_MINIMUM])
+HSV_HIGH = np.uint8([TARGET_HSV_COLOR[0][0][0] + HSV_HUE_RANGE, SV_MAXIMUM, SV_MAXIMUM])
 
 # VPF Preprocessing
 GAUSSIAN_KERNEL_WIDTH = 9  # 15
@@ -51,3 +60,23 @@ H_MARGIN = 1  # 10
 W_MARGIN = 1  # 10
 PHI_START = - (FOV / 2)  # * pi  # -0.5394 * pi
 PHI_END = (FOV / 2)  # pi  # 0.5394 * pi
+
+# Fisheye lens approximate horizontal correction
+# offsets: in pixel from left and right (with input resolution width 320px)
+orig_img_width = 320  # we should get it from contrib.camera but can stay like this for now
+# LENS1
+lens1 = {
+    'offset_left': 0,
+    'offset_right': 28,
+    'a_nonlin': 1 * np.pi,
+    'a_lin': 10,
+    'offset_lin': 0,
+    'h_domain_orig': np.linspace(PHI_START, PHI_END, orig_img_width)
+}
+lens1 = calculate_reverse_mapping_fn(lens1, orig_img_width)
+
+
+# connect robots with lenses (labelled on HW elements with Ri and Li)
+LENS_CONFIG={
+    'Robot1': lens1
+}
