abe9c7fc960790e2c439953fff94bb27b592d0bc
mezdahun <mezeydavid@gmail.com>
2021-08-02 13:35:44 +0200

    added smooth random walk

--- visualswarm/behavior/behavior.py
+++ visualswarm/behavior/behavior.py
@@ -77,9 +77,9 @@ def VPF_to_behavior(VPF_stream, control_stream, motor_control_mode_stream, with_
             dv, dpsi = statevarcomp.compute_state_variables(v, phi, projection_field)
 
             ## TODO: this is temporary smooth reandom walk
-            if np.mean(projection_field) == 0:
-                logger.info('Smoot RW')
-                dpsi += np.random.uniform(-0.8, 0.8, 1)
+            # if np.mean(projection_field) == 0:
+            #     logger.info('Smoot RW')
+            #     dpsi += np.random.uniform(-0.8, 0.8, 1)
 
             logger.warning(dpsi)
             if is_initialized:
