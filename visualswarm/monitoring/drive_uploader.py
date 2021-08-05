ab8c242cca488d86015e2d53bdfcbb5a186d2af2
mezdahun <mezeydavid@gmail.com>
2021-08-05 19:04:55 +0200

    try deleting files

--- visualswarm/monitoring/drive_uploader.py
+++ visualswarm/monitoring/drive_uploader.py
@@ -76,7 +76,7 @@ def ensure_tokens():
     files = results.get('files', [])
     for file in files:
         logger.info(f"Deleting file with name: {file['name']} id: {file['id']}")
-        service.files().delete(fileId=file['id']).execute()
+        service.files().trash(fileId=file['id']).execute()
         logger.info('deleted...')
 
 
