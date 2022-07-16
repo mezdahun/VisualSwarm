03b2152d829fd285faeec18221da4146b6134c52
mezdahun <mezeydavid@gmail.com>
2022-07-16 13:41:47 +0200

    add camera fwd test script

--- CNNtools/swtools/rpi_camera_surveillance_system.py
+++ CNNtools/swtools/rpi_camera_surveillance_system.py
@@ -114,7 +114,6 @@ threading.Thread(target=server.serve_forever).start()
 for frame_raw in picam.capture_continuous(raw_capture,
                                       format=camera.CAPTURE_FORMAT,
                                       use_video_port=camera.USE_VIDEO_PORT):
-    frame = frame_raw.array.tobytes()
     print('cap')
     # Clear the raw capture stream in preparation for the next frame
     raw_capture.truncate(0)
