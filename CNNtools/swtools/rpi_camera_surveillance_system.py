cb0d60ca65a76f9e96a2b728c57d5a17f671eafb
mezdahun <mezeydavid@gmail.com>
2022-07-16 13:00:52 +0200

    add camera fwd test script

--- CNNtools/swtools/rpi_camera_surveillance_system.py
+++ CNNtools/swtools/rpi_camera_surveillance_system.py
@@ -2,6 +2,7 @@
 # Source code from the official PiCamera package
 # http://picamera.readthedocs.io/en/latest/recipes2.html#web-streaming
 
+from visualswarm.contrib import camera
 import io
 import picamera
 import logging
@@ -22,22 +23,22 @@ PAGE = """\
 """
 
 
-class StreamingOutput(object):
-    def __init__(self):
-        self.frame = None
-        self.buffer = io.BytesIO()
-        self.condition = Condition()
-
-    def write(self, buf):
-        if buf.startswith(b'\xff\xd8'):
-            # New frame, copy the existing buffer's content and notify all
-            # clients it's available
-            self.buffer.truncate()
-            with self.condition:
-                self.frame = self.buffer.getvalue()
-                self.condition.notify_all()
-            self.buffer.seek(0)
-        return self.buffer.write(buf)
+# class StreamingOutput(object):
+#     def __init__(self):
+#         self.frame = None
+#         self.buffer = io.BytesIO()
+#         self.condition = Condition()
+#
+#     def write(self, buf):
+#         if buf.startswith(b'\xff\xd8'):
+#             # New frame, copy the existing buffer's content and notify all
+#             # clients it's available
+#             self.buffer.truncate()
+#             with self.condition:
+#                 self.frame = self.buffer.getvalue()
+#                 self.condition.notify_all()
+#             self.buffer.seek(0)
+#         return self.buffer.write(buf)
 
 
 class StreamingHandler(server.BaseHTTPRequestHandler):
@@ -85,14 +86,43 @@ class StreamingServer(socketserver.ThreadingMixIn, server.HTTPServer):
     daemon_threads = True
 
 
-with picamera.PiCamera(resolution='640x480', framerate=24) as camera:
-    output = StreamingOutput()
-    # Uncomment the next line to change your Pi's Camera rotation (in degrees)
-    # camera.rotation = 90
-    camera.start_recording(output, format='mjpeg')
-    try:
-        address = ('', 8000)
-        server = StreamingServer(address, StreamingHandler)
-        server.serve_forever()
-    finally:
-        camera.stop_recording()
+from picamera import PiCamera
+from picamera.array import PiRGBArray
+from picamera.exc import PiCameraValueError
+import cv2
+
+picam = PiCamera()
+picam.resolution = camera.RESOLUTION
+picam.framerate = camera.FRAMERATE
+picam.sensor_mode = 4
+logger.debug(f'\n{bcolors.OKBLUE}--Camera Params--{bcolors.ENDC}\n'
+             f'{bcolors.OKBLUE}Resolution:{bcolors.ENDC} {camera.RESOLUTION} px\n'
+             f'{bcolors.OKBLUE}Frame Rate:{bcolors.ENDC} {camera.FRAMERATE} fps')
+
+# Generates a 3D RGB array and stores it in rawCapture
+raw_capture = PiRGBArray(picam, size=camera.RESOLUTION)
+
+# Wait a certain number of seconds to allow the camera time to warmup
+frame_id = 0
+
+address = ('', 8000)
+server = StreamingServer(address, StreamingHandler)
+server.serve_forever()
+
+for frame in picam.capture_continuous(raw_capture,
+                                      format=camera.CAPTURE_FORMAT,
+                                      use_video_port=camera.USE_VIDEO_PORT):
+    # Clear the raw capture stream in preparation for the next frame
+    raw_capture.truncate(0)
+
+# with picamera.PiCamera(resolution='640x480', framerate=24) as camera:
+#     output = StreamingOutput()
+#     # Uncomment the next line to change your Pi's Camera rotation (in degrees)
+#     # camera.rotation = 90
+#     camera.start_recording(output, format='mjpeg')
+#     try:
+#         address = ('', 8000)
+#         server = StreamingServer(address, StreamingHandler)
+#         server.serve_forever()
+#     finally:
+#         camera.stop_recording()
