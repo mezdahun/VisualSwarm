878a36cc08f30c6ae63165fb683ac37378c7ab32
mezdahun <mezeydavid@gmail.com>
2022-07-16 15:05:09 +0200

    add camera fwd test script

--- CNNtools/swtools/rpi_camera_surveillance_system.py
+++ CNNtools/swtools/rpi_camera_surveillance_system.py
@@ -103,7 +103,7 @@ raw_capture = PiRGBArray(picam, size=camera.RESOLUTION)
 frame_id = 0
 
 address = ('', 8000)
-server = StreamingServer(address, lambda x: Class_Factory('test'))
+server = StreamingServer(address, lambda x, y, z: Class_Factory('test'))
 threading.Thread(target=server.serve_forever).start()
 
 for frame_raw in picam.capture_continuous(raw_capture,
