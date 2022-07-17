2f7dc734553458cfbb255c5312f6f268d8ef9210
mezdahun <mezeydavid@gmail.com>
2022-07-17 15:18:05 +0200

    add webcam code and entrypoint

--- visualswarm/contrib/webcamera.py
+++ visualswarm/contrib/webcamera.py
@@ -4,8 +4,8 @@
 """
 import os
 # Basic parameters
-RESOLUTION = (3280, 2464)
-FRAMERATE = 3
+RESOLUTION = (160, 90)
+FRAMERATE = 30
 CAPTURE_FORMAT = "bgr"
 USE_VIDEO_PORT = True
 FLIP_CAMERA = bool(int(os.getenv('FLIP_CAMERA', '0')))
