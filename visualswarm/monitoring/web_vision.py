c413222b4b88a6865624a10af7cb04be78dac323
mezdahun <mezeydavid@gmail.com>
2022-07-17 12:34:01 +0200

    add webcam code and entrypoint

--- visualswarm/monitoring/web_vision.py
+++ visualswarm/monitoring/web_vision.py
@@ -125,5 +125,7 @@ def start_webcam_only(port=8000):
                                           use_video_port=webcamera.USE_VIDEO_PORT):
         img = frame.array
         raw_capture.truncate(0)
-        print(raw_vision_stream.qsize())
+        if raw_vision_stream.qsize() > 20:
+            raw_vision_stream.get()
         raw_vision_stream.put((img, None, frame_id, None))
+        print(raw_vision_stream.qsize())
