83caa30e3227e42239190ad5488c29dd133eead9
mezdahun <mezeydavid@gmail.com>
2022-07-21 10:08:27 +0200

    limiting max turning

--- visualswarm/vision/vprocess.py
+++ visualswarm/vision/vprocess.py
@@ -610,7 +610,7 @@ def high_level_vision_CNN_calib(raw_vision_stream, high_level_vision_stream, vis
                             else:
                                 box_color = (0, 10, 255)
                             cv2.rectangle(frame_rgb, (xmin_orig, ymin), (xmax_orig, ymax), box_color, 2)
-                            frame_rgb = cv2.putText(frame_rgb, f'{scores[i]:.2f}', (xmin_extend, ymin), cv2.FONT_HERSHEY_SIMPLEX,
+                            frame_rgb = cv2.putText(frame_rgb, f'{scores[i]:.2f}', (xmin_orig, ymin), cv2.FONT_HERSHEY_SIMPLEX,
                                                 0.5, (255, 0, 0), 2, cv2.LINE_AA)
 
                     t3 = datetime.utcnow()
