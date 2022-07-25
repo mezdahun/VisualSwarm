26a8024eab1723656233d2dd8988fe583d1b4854
mezdahun <mezeydavid@gmail.com>
2022-07-25 12:03:38 +0200

    2 class behavior

--- visualswarm/behavior/behavior.py
+++ visualswarm/behavior/behavior.py
@@ -83,8 +83,8 @@ def VPF_to_behavior(VPF_stream, control_stream, motor_control_mode_stream, with_
             #v = 0 # only to measure equilibrium distance. set v0 to zero too
             dv, dpsi = statevarcomp.compute_state_variables(v, phi, projection_field)
             dvc2, dpsic2 = statevarcomp.compute_state_variables(v, phi, projection_field_c2,
-                                                                V0=80, ALP0=200, BET0=10,
-                                                                ALP1=0.00155, BET1=0.00155)
+                                                                V0=80, ALP0=200, BET0=15,
+                                                                ALP1=0.00165, BET1=0.00165)
 
             if v > 0:
                 v = min(v, 300)
--- visualswarm/vision/vprocess.py
+++ visualswarm/vision/vprocess.py
@@ -429,7 +429,7 @@ def high_level_vision_CNN_calib(raw_vision_stream, high_level_vision_stream, vis
             from tflite_runtime.interpreter import load_delegate
 
     min_conf_threshold_class_1 = 0.2
-    min_conf_threshold_class_0= 0.3
+    min_conf_threshold_class_0 = 0.25
     # shoe
     max_num_detection_class_0 = 2
     # robot
