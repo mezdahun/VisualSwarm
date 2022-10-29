0f1933315892328814caa8c360b814404ec5ac9d
mezdahun <mezeydavid@gmail.com>
2022-10-29 15:41:28 +0200

    add new robotsto vision summary

--- visualswarm/monitoring/generate_vision_summary_HTML.py
+++ visualswarm/monitoring/generate_vision_summary_HTML.py
@@ -18,6 +18,11 @@ function AutoChangeImage(){{
     document.getElementById('image_r3').src="http://{HOSTS["Robot3"]}:8000/"+ Date.now() +"stream.mjpg";
     document.getElementById('image_r4').src="http://{HOSTS["Robot4"]}:8000/"+ Date.now() +"stream.mjpg";
     document.getElementById('image_r5').src="http://{HOSTS["Robot5"]}:8000/"+ Date.now() +"stream.mjpg";
+    document.getElementById('image_r6').src="http://{HOSTS["Robot6"]}:8000/"+ Date.now() +"stream.mjpg";
+    document.getElementById('image_r7').src="http://{HOSTS["Robot7"]}:8000/"+ Date.now() +"stream.mjpg";
+    document.getElementById('image_r8').src="http://{HOSTS["Robot8"]}:8000/"+ Date.now() +"stream.mjpg";
+    document.getElementById('image_r9').src="http://{HOSTS["Robot9"]}:8000/"+ Date.now() +"stream.mjpg";
+    document.getElementById('image_r10').src="http://{HOSTS["Robot10"]}:8000/"+ Date.now() +"stream.mjpg";
 }}
 window.onload = function () {{
     setInterval(AutoChangeImage, 1000);
@@ -57,7 +62,27 @@ window.onload = function () {{
 <td class="tg-0lax"><center>
 <h1>Robot5</h1>
 </center><center><img id="image_r5" width="100%" height="100%" /></center></td>
+<td class="tg-0lax"><center>
+<h1>Robot6</h1>
+</center><center><img id="image_r6" width="100%" height="100%" /></center></td>
 <td class="tg-0lax">&nbsp;</td>
+</tr>
+<tr>
+<td class="tg-0lax"><center>
+<h1>Robot7</h1>
+</center><center><img id="image_r7" width="100%" height="100%" /></center></td>
+<td class="tg-0lax"><center>
+<h1>Robot8</h1>
+</center><center><img id="image_r8" width="100%" height="100%" /></center></td>
+<td class="tg-0lax"><center>
+<h1>Robot9</h1>
+</center><center><img id="image_r9" width="100%" height="100%" /></center></td>
+<td class="tg-0lax">&nbsp;</td>
+</tr>
+<tr>
+<td class="tg-0lax"><center>
+<h1>Robot10</h1>
+</center><center><img id="image_r10" width="100%" height="100%" /></center></td>
 </tr>
 </tbody>
 </table>
