090a9cecb6740930f08552b9ffd4ec998dce11eb
mezdahun <mezeydavid@gmail.com>
2022-11-10 17:00:35 +0100

    new IP addresses for robots

--- visualswarm/contrib/puppetmaster.py
+++ visualswarm/contrib/puppetmaster.py
@@ -6,18 +6,22 @@ example command on robot: ENABLE_CLOUD_LOGGING=0 ENABLE_CLOUD_STORAGE=0 SAVE_VIS
 FLIP_CAMERA=0 ROBOT_FOV=3.8 LOG_LEVEL=DEBUG SAVE_CNN_TRAINING_DATA=0 ROBOT_NAME=Robot2 vswrm-start-vision
 """
 
-selected_robots = [3, 8]
+# Select only a few robots, uncomment
+# selected_robots = [3, 8]
 
-ALL_HOSTS = {'Robot1': '192.168.100.162',
-             'Robot2': '192.168.100.179',
-             'Robot3': '192.168.100.131',
-             'Robot4': '192.168.100.170',
-             'Robot5': '192.168.100.181',
-             'Robot6': '192.168.100.163',
-             'Robot7': '192.168.100.112',
-             'Robot8': '192.168.100.165',
-             'Robot9': '192.168.100.120',
-             'Robot10': '192.168.100.108'}
+# select all robots uncomment this line
+selected_robots = [i+1 for i in range(10)]
+
+ALL_HOSTS = {'Robot1': '192.168.0.194',
+             'Robot2': '192.168.0.176',
+             'Robot3': '192.168.0.173',
+             'Robot4': '192.168.0.110',
+             'Robot5': '192.168.0.177',
+             'Robot6': '192.168.0.122',
+             'Robot7': '192.168.0.153',
+             'Robot8': '192.168.0.169',
+             'Robot9': '192.168.0.145',
+             'Robot10': '192.168.0.103'}
 
 HOSTS = {}
 for rid in selected_robots:
