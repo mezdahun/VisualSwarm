db970c917e1dc4a0e215c93501cd3f6b5fb4521e
mezdahun <mezeydavid@gmail.com>
2022-12-08 10:20:28 +0100

    divided vision test

--- visualswarm/vision/vprocess.py
+++ visualswarm/vision/vprocess.py
@@ -653,8 +653,8 @@ def high_level_vision_CNN_calib(raw_vision_stream, high_level_vision_stream, vis
                         # creating an individual projection field per each blob
                         logger.debug("Divided retina per blob is requested")
                         print(len(scores))
-                        print(min(1, len(scores)))
-                        blurred_divided = np.zeros([img.shape[0], img.shape[1] + 2*img.shape[0], min(1, len(scores))])
+                        print(max(1, len(scores)))
+                        blurred_divided = np.zeros([img.shape[0], img.shape[1] + 2*img.shape[0], max(1, len(scores))])
                         print(blurred_divided.shape)
 
                     for i in range(len(scores)):
