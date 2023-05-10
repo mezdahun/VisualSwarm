65c3eab1911703d2a5351a1aa40fc85f6b30d434
mezdahun <mezeydavid@gmail.com>
2023-05-10 15:25:02 +0200

    added velocity sign and increased turning speed

--- visualswarm/control/motoroutput.py
+++ visualswarm/control/motoroutput.py
@@ -134,7 +134,7 @@ def hardlimit_motor_speed(v_left: float, v_right: float) -> list:
     return [v_left_lim, v_right_lim]
 
 
-def distribute_overall_speed(v: float, dpsi: float, v_lower_thr=10, dpsi_p_threshold=0.05) -> list:
+def distribute_overall_speed(v: float, dpsi: float, v_lower_thr=10, dpsi_p_threshold=0.025) -> list:
     """
     distributing desired forward speed to motor velocities according to the change in the heading angle dpsi.
         Args:
@@ -149,9 +149,9 @@ def distribute_overall_speed(v: float, dpsi: float, v_lower_thr=10, dpsi_p_thres
 
     if v < v_lower_thr and dpsi_p > dpsi_p_threshold:
         # stationary turn due to large angle and low speed
-        v_turn = 100
-        v_left = (v_turn/2) * dpsi_p
-        v_right = -(v_turn/2) * dpsi_p
+        v_turn = 200
+        v_left = np.sign(v) * (v_turn/2) * dpsi_p
+        v_right = -(v_turn/2) * dpsi_p * np.sign(v)
 
     else:
         # Matching simulation scale with reality
