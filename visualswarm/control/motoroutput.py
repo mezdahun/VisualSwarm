78da331f22571912dd35ecd489453bd299ec042c
mezdahun <mezeydavid@gmail.com>
2023-05-10 15:07:34 +0200

    introduce psi threshold

--- visualswarm/control/motoroutput.py
+++ visualswarm/control/motoroutput.py
@@ -134,7 +134,7 @@ def hardlimit_motor_speed(v_left: float, v_right: float) -> list:
     return [v_left_lim, v_right_lim]
 
 
-def distribute_overall_speed(v: float, dpsi: float, v_lower_thr=10) -> list:
+def distribute_overall_speed(v: float, dpsi: float, v_lower_thr=10, dpsi_p_threshold=0.1) -> list:
     """
     distributing desired forward speed to motor velocities according to the change in the heading angle dpsi.
         Args:
@@ -147,18 +147,19 @@ def distribute_overall_speed(v: float, dpsi: float, v_lower_thr=10) -> list:
     # Calculating proportional heading angle change
     dpsi_p = dpsi / np.pi
 
-    if v > v_lower_thr:
+    if v < v_lower_thr and dpsi_p > dpsi_p_threshold:
+        # stationary turn due to large angle and low speed
+        v_turn = 50
+        v_left = (v_turn/2) * dpsi_p
+        v_right = -(v_turn/2) * dpsi_p
+
+    else:
         # Matching simulation scale with reality
         v = v * behavior.KAP
 
         # Distributing velocity
         v_left = v * (1 + dpsi_p)
         v_right = v * (1 - dpsi_p)
-
-    else:
-        v_turn = 30
-        v_left = (v_turn/2) * dpsi_p
-        v_right = -(v_turn/2) * dpsi_p
 
 
     return [v_left, v_right]
