8390a3a6753dc89b2fa1023442510e3ec6f3fe8a
mezdahun <mezeydavid@gmail.com>
2023-05-20 13:48:15 +0200

    #tmp get_latest element

--- visualswarm/behavior/behavior.py
+++ visualswarm/behavior/behavior.py
@@ -139,7 +139,7 @@ def VPF_to_behavior(VPF_stream, control_stream, motor_control_mode_stream, with_
             if v > 0:
                 v = min(v, 160)
             elif v < 0:
-                v = max(v, 0)
+                v = max(v, -20)
 
             v = float(v)
 
