b1f6953f0bd939e9d374b72257c19dd7708c03a6
mezdahun <mezeydavid@gmail.com>
2023-12-11 13:54:53 +0100

    introducing stationary turning and limited backwards movement speed

--- visualswarm/control/motoroutput.py
+++ visualswarm/control/motoroutput.py
@@ -179,8 +179,8 @@ def distribute_overall_speed(v: float, dpsi: float, v_thr=20) -> list:
             # stationary turn due to large angle and low speed
             print(f"velocity: {v}")
             print(f"prop. angle change: {dpsi_p}")
-            v_left = algoimp.MAX_BACKWARDS_SPEED + algoimp.STAT_TURN_SPEED * dpsi_p * np.sign(v)
-            v_right = algoimp.MAX_BACKWARDS_SPEED - algoimp.STAT_TURN_SPEED * dpsi_p * np.sign(v)
+            v_left = - algoimp.MAX_BACKWARDS_SPEED + algoimp.STAT_TURN_SPEED * dpsi_p * np.sign(v)
+            v_right = - algoimp.MAX_BACKWARDS_SPEED - algoimp.STAT_TURN_SPEED * dpsi_p * np.sign(v)
         elif np.abs(v) < algoimp.STAT_TURN_VEL_THRES:
             # stationary turn due to large angle and low speed
             print(f"velocity: {v}")
