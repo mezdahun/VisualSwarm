181c53bec91f92382fe27781a76ccbdd89b9c79c
mezdahun <mezeydavid@gmail.com>
2023-12-12 13:11:27 +0100

    limite backward turning

--- visualswarm/control/motoroutput.py
+++ visualswarm/control/motoroutput.py
@@ -184,8 +184,8 @@ def distribute_overall_speed(v: float, dpsi: float, v_thr=20) -> list:
             print(f"velocity: {v}")
             print(f"prop. angle change: {dpsi_p}")
             dpsi_p = np.sign(dpsi_p) * max(np.abs(dpsi_p), 0.5)
-            v_left = - algoimp.MAX_BACKWARDS_SPEED + algoimp.STAT_TURN_SPEED_BACK * dpsi_p * np.sign(v)
-            v_right = - algoimp.MAX_BACKWARDS_SPEED - algoimp.STAT_TURN_SPEED_BACK * dpsi_p * np.sign(v)
+            v_left = - algoimp.MAX_BACKWARDS_SPEED + algoimp.STAT_TURN_SPEED_BACK * dpsi_p
+            v_right = - algoimp.MAX_BACKWARDS_SPEED - algoimp.STAT_TURN_SPEED_BACK * dpsi_p
         elif np.abs(v) < algoimp.STAT_TURN_VEL_THRES:
             # stationary turn due to large angle and low speed
             print(f"velocity: {v}")
